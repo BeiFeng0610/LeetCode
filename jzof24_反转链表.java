c82a87d3aae4aeb305679b1858c7dceea15e427f
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-03-24 15:21:35 +0800

    Create jzof24_反转链表.java
    双指针

---
+++ jzof24_反转链表.java
@@ -0,0 +1,12 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode cur = head, pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
