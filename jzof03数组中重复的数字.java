54f7693a1e6504cf774fb74154f345f6bae4ce1e
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-03-25 17:45:52 +0800

    Create jzof03数组中重复的数字.java

---
+++ jzof03数组中重复的数字.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findRepeatNumber(int[] nums) {
+        HashSet<Integer> set = new HashSet<>();
+        int len = nums.length;
+        for(int i = 0; i < len; i++){
+            if(set.contains(nums[i])){
+                return nums[i];
+            }
+            set.add(nums[i]);
+        }
+        return 0;
+    }
+}
