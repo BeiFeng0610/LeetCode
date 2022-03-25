92e7d7ef87edab1489e061f6edbbdc7c2a22ce18
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-03-25 17:44:49 +0800

    Create jzof58II左旋转字符串.java

---
+++ jzof58II左旋转字符串.java
@@ -0,0 +1,8 @@
+class Solution {
+    public String reverseLeftWords(String s, int n) {
+        String res = "";
+        for(int i = n; i < n + s.length(); i++)
+            res += s.charAt(i % s.length());
+        return res;
+    }
+}
