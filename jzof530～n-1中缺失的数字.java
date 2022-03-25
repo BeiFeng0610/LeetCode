edbf79b2b9c293eb12de89a99dccf7d863de4aea
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-03-25 17:47:28 +0800

    Create jzof530～n-1中缺失的数字.java

---
+++ jzof530～n-1中缺失的数字.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        int len = nums.length;
+        int l = 0, r = len - 1;
+        while (l <= r) {
+            int mid = (r - l) / 2 + l;
+            if (nums[mid] == mid) {
+                l = mid + 1;
+            } else {
+                r = mid - 1;
+            }
+
+        }
+        return l;
+    }
+}
