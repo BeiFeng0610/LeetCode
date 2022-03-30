01824739707d55ed6c9a2e7196421003a841842a
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-03-30 15:32:13 +0800

    Create jzof47_礼物的最大价值.java

---
+++ jzof47_礼物的最大价值.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxValue(int[][] grid) {
+        int hang = grid.length;
+        int lie = grid[0].length;
+        int[][] li = new int[hang][lie];
+        li[0][0] = grid[0][0];
+        for (int i = 1; i < lie; i++) {
+            li[0][i] = li[0][i - 1] + grid[0][i];
+        }
+        for (int i = 1; i < hang; i++) {
+            li[i][0] = li[i - 1][0] + grid[i][0];
+        }
+        for (int i = 1; i < hang; i++) {
+            for (int j = 1; j < lie; j++) {
+                li[i][j] = Math.max(li[i - 1][j], li[i][j - 1]) + grid[i][j];
+            }
+        }
+
+        return li[hang - 1][lie - 1];
+    }
+}
