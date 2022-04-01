3e5bc14a31e55a501d9d9d7b598735ccf555f1cd
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-01 15:25:52 +0800

    Create jzof52_两个链表的第一个公共节点.java

---
+++ jzof52_两个链表的第一个公共节点.java
@@ -0,0 +1,10 @@
+public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode A = headA, B = headB;
+        while (A != B) {
+            A = A != null ? A.next : headB;
+            B = B != null ? B.next : headA;
+        }
+        return A;
+    }
+}
