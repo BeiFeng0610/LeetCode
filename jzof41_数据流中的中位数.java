e02bbfa116fa41c1348adeeaf6d761647d36dbb1
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-06 15:28:23 +0800

    Create jzof41_数据流中的中位数.java

---
+++ jzof41_数据流中的中位数.java
@@ -0,0 +1,19 @@
+class MedianFinder {
+    Queue<Integer> A, B;
+    public MedianFinder() {
+        A = new PriorityQueue<>(); // 小顶堆，保存较大的一半
+        B = new PriorityQueue<>((x, y) -> (y - x)); // 大顶堆，保存较小的一半
+    }
+    public void addNum(int num) {
+        if(A.size() != B.size()) {
+            A.add(num);
+            B.add(A.poll());
+        } else {
+            B.add(num);
+            A.add(B.poll());
+        }
+    }
+    public double findMedian() {
+        return A.size() != B.size() ? A.peek() : (A.peek() + B.peek()) / 2.0;
+    }
+}
