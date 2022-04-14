1400c648c9bf2cbbc66e20418a96186c107543cc
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-14 13:55:54 +0800

    Create jzof31. 栈的压入、弹出序列.java

---
+++ jzof31. 栈的压入、弹出序列.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean validateStackSequences(int[] pushed, int[] popped) {
+        Deque<Integer> stack = new LinkedList<>();
+        int i = 0;
+        for (int num : pushed) {
+            stack.push(num);
+            while (!stack.isEmpty() && popped[i] == stack.peek()) {
+                stack.pop();
+                i++;
+            }
+        }
+        return stack.isEmpty();
+    }
+}
