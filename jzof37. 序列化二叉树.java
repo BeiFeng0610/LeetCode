6c042e880e14637f4428eded470685ac1c2b193d
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-17 13:13:49 +0800

    Create jzof37. 序列化二叉树.java

---
+++ jzof37. 序列化二叉树.java
@@ -0,0 +1,45 @@
+class Codec {
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        if (root == null) return "[]";
+        StringBuilder res = new StringBuilder("[");
+        Queue<TreeNode> queue = new LinkedList<>() {{
+            add(root);
+        }};
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                res.append(node.val + ",");
+                queue.add(node.left);
+                queue.add(node.right);
+            } else res.append("null,");
+        }
+        res.deleteCharAt(res.length() - 1);
+        res.append("]");
+        return res.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public static TreeNode deserialize(String data) {
+        if (data == "[]") return null;
+        String[] strings = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(strings[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int i = 1;
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!strings[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(strings[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (!strings[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(strings[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
