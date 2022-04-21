a892198097f3c8ee112f726ecd2728c5f63322e2
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-21 23:16:38 +0800

    Update Offer II 003. 前 n 个数字二进制中 1 的个数.java

--- 剑指Office（专项）/Offer II 003. 前 n 个数字二进制中 1 的个数.java
+++ 剑指Office（专项）/Offer II 003. 前 n 个数字二进制中 1 的个数.java
@@ -1,3 +1,4 @@
+// 👴自己的笨方法
 class Solution {
     public int[] countBits(int n) {
         int[] ints = new int[n + 1];
@@ -20,6 +21,15 @@ class Solution2 {
     public int[] countBits(int n) {
         int[] bits = new int[n + 1];
         int highBit = 0;
+        
+        /*
+            有效高位
+                每次碰到单1是例如，1 10 100 1000，总会重置为一次
+                然后重新从下标0开始统计，例如
+                    1001=1000的位数 + 1的位数
+                    1010=1000的位数 + 10的位数
+                 
+        */
         for (int i = 1; i <= n; i++) {
             if ((i & (i - 1)) == 0) {
                 highBit = i;
@@ -33,6 +43,8 @@ class Solution2 {
 class Solution3 {
     public int[] countBits(int n) {
         int[] bits = new int[n + 1];
+        // 101 比 10多一位，获取多的一位是1还是0
+        // 111 比 11多一位，获取多的一位是1还是0
         for (int i = 1; i <= n; i++) {
             bits[i] = bits[i >> 1] + (i & 1);
         }
