95b08a69a4fe4aba7793907261ea3c8f2b49fe76
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-21 23:07:30 +0800

    Update Offer II 002. 二进制加法.java

--- 剑指Office（专项）/Offer II 002. 二进制加法.java
+++ 剑指Office（专项）/Offer II 002. 二进制加法.java
@@ -4,10 +4,10 @@ class Solution {
 
         int n = Math.max(a.length(), b.length()), carry = 0;
         for (int i = 0; i < n; ++i) {
-            // 判断越界，没越界就获取没获取过的最后一位
+            // 判断越界，没越界就获取没获取过的最后一位，为1就统计加1
             carry += i < a.length() ? (a.charAt(a.length() - 1 - i) - '0') : 0;
             carry += i < b.length() ? (b.charAt(b.length() - 1 - i) - '0') : 0;
-            // 判断是否有上一次进位，carry=3 字符串加0
+            // 判断是否有上一次进位，carry=3 字符串加1，继续发生进位
             ans.append((char) (carry % 2 + '0'));
             // 如果 carry>2 发生进位
             carry /= 2;
