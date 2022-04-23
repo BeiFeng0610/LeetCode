b2fca891a987ef1003058549f0cef85c6463cf33
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-24 00:02:28 +0800

    Update Offer II 008. 和大于等于 target 的最短子数组.java

--- 剑指Office（专项）/Offer II 008. 和大于等于 target 的最短子数组.java
+++ 剑指Office（专项）/Offer II 008. 和大于等于 target 的最短子数组.java
@@ -4,13 +4,16 @@ class Solution {
         if(len == 0) return 0;
         int l = 0, r = 0;
         int sum = nums[0], ans = Integer.MAX_VALUE;
+        // 设定边界
         while (l<=r) {
+            // 总数小于，右边界右移
             if (sum < target) {
                 if (r + 1 < len) {
                     sum += nums[++r];
                 } else {
                     break;
                 }
+            // 记录最小长度，左边界右移
             } else {
                 ans = Math.min(ans, r - l + 1);
                 sum -= nums[l++];
