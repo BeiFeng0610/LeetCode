a4caf4239ad0157c6df9419cd463aa7329efba67
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-23 10:24:03 +0800

    Update Offer II 005. 单词长度的最大乘积.java

--- 剑指Office（专项）/Offer II 005. 单词长度的最大乘积.java
+++ 剑指Office（专项）/Offer II 005. 单词长度的最大乘积.java
@@ -2,12 +2,14 @@ class Solution {
     public int maxProduct(String[] words) {
         int wordLen = words.length;
         int[] ans = new int[wordLen];
+        // 统计每个字母出现的位数，转换为二进制。最多出现第26位，不会越界
         for (int i = 0; i < wordLen; i++) {
             String word = words[i];
             for (int j = 0; j < word.length(); j++) {
                 ans[i] |= 1 << word.charAt(j) - 'a';
             }
         }
+        // 两数做 & 运算，如果有位数相同，则存在相同字母。
         int max = 0;
         for (int i = 0; i < wordLen; i++) {
             for (int j = i + 1; j < wordLen; j++) {
