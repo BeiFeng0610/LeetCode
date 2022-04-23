f786452f6a662603d2030b413b5f4d17e157f581
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-23 10:22:26 +0800

    Update Offer II 004. 只出现一次的数字.java

--- 剑指Office（专项）/Offer II 004. 只出现一次的数字.java
+++ 剑指Office（专项）/Offer II 004. 只出现一次的数字.java
@@ -1,12 +1,14 @@
 class Solution {
     public int singleNumber(int[] nums) {
         int[] res = new int[32];
+        // 统计每个位数的出现次数。
         for (int num : nums) {
             for (int i = 0; i < 32; i++) {
                 res[i] += num & 1;
                 num >>= 1;
             }
         }
+        // 如果余数不为0，则提取相应位数
         int ans = 0;
         for (int i = 0; i < 32; i++) {
             if (res[i] % 3 != 0) {
