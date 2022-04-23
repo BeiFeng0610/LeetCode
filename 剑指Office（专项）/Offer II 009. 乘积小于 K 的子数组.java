0f0b854c552d037f7a7d56dc01ff360f8eb94c57
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-24 00:06:23 +0800

    Update Offer II 009. 乘积小于 K 的子数组.java

--- 剑指Office（专项）/Offer II 009. 乘积小于 K 的子数组.java
+++ 剑指Office（专项）/Offer II 009. 乘积小于 K 的子数组.java
@@ -4,14 +4,15 @@ class Solution {
         if (k == 0 || len == 0) {
             return 0;
         }
-
         int count = 0;
         int l = 0, r = 0, num = 1;
+        // 定义左边界，得到最大宽度
         for (r = 0; r < len; r++) {
             num *= nums[r];
             while (l <= r && num >= k){
                 num /= nums[l++];
             }
+            // 记录符合的组合，因为子组合也符合，所以r - l + 1，每次左边界回收都记录累加。
             if (l <= r) {
                 count += r - l + 1;
             }
