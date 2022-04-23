0c1badbd87f65ef6b61e17be549dcf688b1d180c
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-23 23:56:26 +0800

    Update Offer II 007. 数组中和为 0 的三个数.java

--- 剑指Office（专项）/Offer II 007. 数组中和为 0 的三个数.java
+++ 剑指Office（专项）/Offer II 007. 数组中和为 0 的三个数.java
@@ -1,1 +1,36 @@
-
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        int len = nums.length;
+        if (len < 3) {
+            return list;
+        }
+        Arrays.sort(nums);
+        for (int i = 0; i < len; i++) {
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int target = -nums[i];
+            int k = len - 1;
+            for (int j = i + 1; j < len; j++) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
+                    continue;
+                }
+                while (j < k && nums[j] + nums[k] > target) {
+                    --k;
+                }
+                if (k == j) {
+                    break;
+                }
+                if (nums[j] + nums[k] == target) {
+                    List<Integer> tmp = new ArrayList<Integer>();
+                    tmp.add(nums[i]);
+                    tmp.add(nums[j]);
+                    tmp.add(nums[k]);
+                    list.add(tmp);
+                }
+            }
+        }
+        return list;
+    }
+}
