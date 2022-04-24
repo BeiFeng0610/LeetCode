8dda600986fddf31dcef6833ee2211829a673341
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-24 23:57:38 +0800

    Update Offer II 011. 0 和 1 个数相同的子数组.java

--- 剑指Office（专项）/Offer II 011. 0 和 1 个数相同的子数组.java
+++ 剑指Office（专项）/Offer II 011. 0 和 1 个数相同的子数组.java
@@ -3,19 +3,23 @@ class Solution {
         int maxLength = 0;
         Map<Integer, Integer> map = new HashMap<>();
         int counter = 0;
+        // 前缀为0时，下标肯定在-1
         map.put(counter, -1);
         int n = nums.length;
         for (int i = 0; i < n; i++) {
+            // num == 1 时记录0和1的个数差值
             int num = nums[i];
             if (num == 1) {
                 counter++;
             } else {
                 counter--;
             }
+            // counter-- 后，差值减少，找到差值起始下标，相减，记录最大长度。
             if (map.containsKey(counter)) {
                 int prevIndex = map.get(counter);
                 maxLength = Math.max(maxLength, i - prevIndex);
             } else {
+                // 更新最大差值下标。
                 map.put(counter, i);
             }
         }
