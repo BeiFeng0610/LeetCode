2776933c2c3b34a2a5b8ec71c26491cef9eadb98
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-24 23:58:48 +0800

    Update Offer II 012. 左右两边子数组的和相等.java

--- 剑指Office（专项）/Offer II 012. 左右两边子数组的和相等.java
+++ 剑指Office（专项）/Offer II 012. 左右两边子数组的和相等.java
@@ -3,8 +3,9 @@ class Solution {
         int len = nums.length;
         int sum = 0, pre = 0;
         for (int num : nums) {
-            sum += num;
+            sum += num;// 记录总和
         }
+        // 从0开始判断除0下标两边的和，相等则返回
         for (int i = 0; i < len; i++) {
             sum -= nums[i];
             if (sum == pre) {
