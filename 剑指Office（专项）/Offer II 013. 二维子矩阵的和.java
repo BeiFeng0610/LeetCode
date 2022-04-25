09351843f5e13599d103bf0e5ea2632b0c731b7d
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-26 00:06:33 +0800

    Update Offer II 013. 二维子矩阵的和.java

--- 剑指Office（专项）/Offer II 013. 二维子矩阵的和.java
+++ 剑指Office（专项）/Offer II 013. 二维子矩阵的和.java
@@ -7,6 +7,7 @@ class NumMatrix {
     }
 
     private void initPre(int[][] matrix) {
+        // 求出每行前缀和
         for (int i = 0; i < matrix.length; i++) {
             for (int j = 1; j < matrix[0].length; j++) {
                 this.matrix[i][j] += this.matrix[i][j - 1];
@@ -16,6 +17,8 @@ class NumMatrix {
 
     public int sumRegion(int row1, int col1, int row2, int col2) {
         int sum = 0;
+        
+        // 根据行列，直接统计前缀之差
         for (int i = row1; i <= row2; i++) {
             if (col1 > 0) {
                 sum += matrix[i][col2] - matrix[i][col1 - 1];
