52ab548648bc5a13d0d1791d8afa837fd880b78f
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-25 15:01:47 +0800

    Create Offer II 013. 二维子矩阵的和.java

---
+++ 剑指Office（专项）/Offer II 013. 二维子矩阵的和.java
@@ -0,0 +1,28 @@
+class NumMatrix {
+    private int[][] matrix;
+
+    public NumMatrix(int[][] matrix) {
+        this.matrix = matrix;
+        initPre(this.matrix);
+    }
+
+    private void initPre(int[][] matrix) {
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 1; j < matrix[0].length; j++) {
+                this.matrix[i][j] += this.matrix[i][j - 1];
+            }
+        }
+    }
+
+    public int sumRegion(int row1, int col1, int row2, int col2) {
+        int sum = 0;
+        for (int i = row1; i <= row2; i++) {
+            if (col1 > 0) {
+                sum += matrix[i][col2] - matrix[i][col1 - 1];
+            } else {
+                sum += matrix[i][col2];
+            }
+        }
+        return sum;
+    }
+}
