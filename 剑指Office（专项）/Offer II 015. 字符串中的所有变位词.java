9b58f99e2956472baf242464661eb73305aaac9f
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-25 16:25:37 +0800

    Update Offer II 015. 字符串中的所有变位词.java

--- 剑指Office（专项）/Offer II 015. 字符串中的所有变位词.java
+++ 剑指Office（专项）/Offer II 015. 字符串中的所有变位词.java
@@ -24,3 +24,28 @@ class Solution {
         return res;
     }
 }
+
+class Solution2 {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> res = new ArrayList<>();
+        int n = s.length(), m = p.length();
+        if (n < m) {
+            return res;
+        }
+        int[] cnt1 = new int[26];
+        int[] cnt2 = new int[26];
+        for (int i = 0; i < m; i++) {
+            ++cnt1[s.charAt(i) - 'a'];
+            ++cnt2[p.charAt(i) - 'a'];
+        }
+        if (Arrays.equals(cnt1,cnt2)) res.add(0);
+        for (int i = m; i < n; i++) {
+            --cnt1[s.charAt(i - m) - 'a'];
+            ++cnt1[s.charAt(i) - 'a'];
+            if (Arrays.equals(cnt1, cnt2)) {
+                res.add(i - m + 1);
+            }
+        }
+        return res;
+    }
+}
