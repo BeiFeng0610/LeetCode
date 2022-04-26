3ab657d48108a710169da89cfe50f3d2d97a9552
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-26 23:36:08 +0800

    Update Offer II 016. 不含重复字符的最长子字符串.java

--- 剑指Office（专项）/Offer II 016. 不含重复字符的最长子字符串.java
+++ 剑指Office（专项）/Offer II 016. 不含重复字符的最长子字符串.java
@@ -8,10 +8,13 @@ class Solution {
         char[] chars = s.toCharArray();
         int l = 0, r = 0, max = 0;
         for (r = 0; r < len; r++) {
+            // set不包含则存入
             if (!set.contains(chars[r])) {
                 set.add(chars[r]);
             } else {
+                // 存在则记录最大长度
                 max = Math.max(max, r - l);
+                // 判断是那个字符重复，左边界收敛到重复的第一个字符，再右移一位，右边界即可继续右移
                 while (chars[l] != chars[r]) {
                     set.remove(chars[l]);
                     l++;
