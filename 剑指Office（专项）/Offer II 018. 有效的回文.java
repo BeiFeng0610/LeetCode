b83f0c129517f3137ad51b841167bcaa06bb34eb
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-26 23:49:56 +0800

    Update Offer II 018. 有效的回文.java

--- 剑指Office（专项）/Offer II 018. 有效的回文.java
+++ 剑指Office（专项）/Offer II 018. 有效的回文.java
@@ -2,7 +2,9 @@ class Solution {
     public boolean isPalindrome(String s) {
         int len = s.length();
         int l = 0, r = len - 1;
+        // 双指针
         while (l < r) {
+            // 寻找右指针和左指针有效位，比较即可
             while (l < r && !Character.isLetterOrDigit(s.charAt(l))) {
                 l++;
             }
