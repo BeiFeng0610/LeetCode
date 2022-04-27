efed0906346dd628edd38f03c44fb83140cf19b3
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-27 23:19:11 +0800

    Update Offer II 020. 回文子字符串的个数.java

--- 剑指Office（专项）/Offer II 020. 回文子字符串的个数.java
+++ 剑指Office（专项）/Offer II 020. 回文子字符串的个数.java
@@ -1,6 +1,7 @@
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length(), ans = 0;
+        // 中心扩展，中心个数位2*n-1个，例如 abc，中心位为a ab b bc c，因为回文串字母个数可能为偶数位，则中心位为两位。
         for (int i = 0; i < 2 * n - 1; ++i) {
             int l = i / 2, r = i / 2 + i % 2;
             while (l >= 0 && r < n && s.charAt(l) == s.charAt(r)) {
