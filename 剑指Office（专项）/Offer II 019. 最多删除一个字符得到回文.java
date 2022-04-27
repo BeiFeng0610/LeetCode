e0c7789d2246db2aa1155f8d0f939d265959d219
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-27 23:17:27 +0800

    Update Offer II 019. 最多删除一个字符得到回文.java

--- 剑指Office（专项）/Offer II 019. 最多删除一个字符得到回文.java
+++ 剑指Office（专项）/Offer II 019. 最多删除一个字符得到回文.java
@@ -3,6 +3,7 @@ class Solution {
         int l = 0, r = s.length() - 1;
         while (l < r) {
             char c1 = s.charAt(l), c2 = s.charAt(r);
+            // 如果不相等，则删除左边，或者右边一个字母，如果还不是回文，则返回false
             if (c1 != c2) {
                 return isPalindrome(s, l + 1, r) || isPalindrome(s, l, r - 1);
             } else {
