b1453205bdb4c75252720552f95f903ebd55856a
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-28 23:44:54 +0800

    Update Offer II 023. 两个链表的第一个重合节点.java

--- 剑指Office（专项）/Offer II 023. 两个链表的第一个重合节点.java
+++ 剑指Office（专项）/Offer II 023. 两个链表的第一个重合节点.java
@@ -3,6 +3,7 @@ class Solution {
         if (headA == null || headB == null) {
             return null;
         }
+        // 两个链表，pa走a+b步 和pb走a+b步，如果有重合结点，一定会重合一次，如果不重合，和出现都变为null，刚好可以返回null
         ListNode pA = headA, pB = headB;
         while (pA != pB) {
             if (pA == null) {
