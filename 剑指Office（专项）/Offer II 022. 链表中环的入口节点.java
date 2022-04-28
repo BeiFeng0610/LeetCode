0e0860d458940e0108dcc25a9dc7e06d65dde265
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-28 23:42:58 +0800

    Update Offer II 022. 链表中环的入口节点.java

--- 剑指Office（专项）/Offer II 022. 链表中环的入口节点.java
+++ 剑指Office（专项）/Offer II 022. 链表中环的入口节点.java
@@ -3,6 +3,8 @@ class Solution {
         if (head == null) {
             return null;
         }
+        // 快慢指针，慢走1，快走2，如果有环，快慢碰见的距离：假设慢走x，快就走2x
+        // 设开始结点到入口结点为y，则慢指针再走y步也能到入口，因为慢结点要走x才能到现在的位置，初始结点到现在的位置也是x步，减去重合部分，所以初始结点y步和慢指针y步刚好重合
         ListNode slow = head, fast = head;
         while (fast != null) {
             slow = slow.next;
@@ -24,7 +26,7 @@ class Solution {
     }
 }
 
-
+// 哈希表
 class Solution2 {
     public ListNode detectCycle(ListNode head) {
         if (head == null) {
