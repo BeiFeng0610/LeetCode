f0f2faa63bf4c44b9e960def608d69cdfb203699
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-28 11:19:08 +0800

    Create Offer II 023. 两个链表的第一个重合节点.java

---
+++ 剑指Office（专项）/Offer II 023. 两个链表的第一个重合节点.java
@@ -0,0 +1,21 @@
+class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode pA = headA, pB = headB;
+        while (pA != pB) {
+            if (pA == null) {
+                pA = headB;
+            } else {
+                pA = pA.next;
+            }
+            if (pB == null) {
+                pB = headA;
+            } else {
+                pB = pB.next;
+            }
+        }
+        return pA;
+    }
+}
