51bf9ff3d1f995f36c9eae7151d2e5a83403379d
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-29 12:51:51 +0800

    Create Offer II 027. 回文链表.java

---
+++ 剑指Office（专项）/Offer II 027. 回文链表.java
@@ -0,0 +1,98 @@
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while (head != null) {
+            sb.append(head.val);
+            head = head.next;
+        }
+        String s = sb.toString();
+        String reverse = sb.reverse().toString();
+        return s.equals(reverse);
+    }
+}
+
+class Solution2 {
+    public boolean isPalindrome(ListNode head) {
+        ListNode mid = midNode(head);
+        ListNode l1 = head;
+        ListNode l2 = reverseList(mid.next);
+        mid.next = null;
+        while (l2 != null) {
+            if (l1.val != l2.val) {
+                return false;
+            }
+            l1 = l1.next;
+            l2 = l2.next;
+        }
+        return true;
+    }
+
+    public ListNode midNode(ListNode root) {
+        ListNode slow = root;
+        ListNode fast = root;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
+    }
+
+    public ListNode reverseList(ListNode root) {
+        ListNode head = new ListNode(-1);
+        ListNode tmp = null;
+        while (root != null) {
+            tmp = root;
+            root = root.next;
+            tmp.next = head.next;
+            head.next = tmp;
+        }
+        return head.next;
+    }
+}
+
+class Solution3 {
+    public boolean isPalindrome(ListNode head) {
+        List<Integer> vals = new ArrayList<Integer>();
+
+        // 将链表的值复制到数组中
+        ListNode currentNode = head;
+        while (currentNode != null) {
+            vals.add(currentNode.val);
+            currentNode = currentNode.next;
+        }
+
+        // 使用双指针判断是否回文
+        int front = 0;
+        int back = vals.size() - 1;
+        while (front < back) {
+            if (!vals.get(front).equals(vals.get(back))) {
+                return false;
+            }
+            front++;
+            back--;
+        }
+        return true;
+    }
+}
+
+class Solution4 {
+    private ListNode frontPointer;
+
+    private boolean recursivelyCheck(ListNode currentNode) {
+        if (currentNode != null) {
+            if (!recursivelyCheck(currentNode.next)) {
+                return false;
+            }
+            if (currentNode.val != frontPointer.val) {
+                return false;
+            }
+            frontPointer = frontPointer.next;
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(ListNode head) {
+        frontPointer = head;
+        return recursivelyCheck(head);
+    }
+}
