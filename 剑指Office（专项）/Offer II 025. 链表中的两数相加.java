46d1dbb2c19538d8869c07a4981ddc3ef9d751c1
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-29 10:10:34 +0800

    Create Offer II 025. 链表中的两数相加.java

---
+++ 剑指Office（专项）/Offer II 025. 链表中的两数相加.java
@@ -0,0 +1,31 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Deque<ListNode> stack1 = new LinkedList<>();
+        Deque<ListNode> stack2 = new LinkedList<>();
+        while (l1 != null) {
+            stack1.push(l1);
+            l1 = l1.next;
+        }
+        while (l2 != null) {
+            stack2.push(l2);
+            l2 = l2.next;
+        }
+        ListNode res = new ListNode(-1);
+        int jin = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || jin != 0) {
+            ListNode node = new ListNode();
+            int n1 = stack1.peek() == null ? 0 : stack1.pop().val;
+            int n2 = stack2.peek() == null ? 0 : stack2.pop().val;
+            int num = n1 + n2 + jin;
+            if (num < 10) {
+                node.val = num;
+            } else {
+                node.val = num - 10;
+            }
+            node.next = res.next;
+            res.next = node;
+            jin = num >= 10 ? 1 : 0;
+        }
+        return res.next;
+    }
+}
