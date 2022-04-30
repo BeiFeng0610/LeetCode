691882d4a3f000a6698c5970895ba3394468e069
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-01 00:23:54 +0800

    Update Offer II 030. 插入、删除和随机访问都是 O(1) 的容器.java

--- 剑指Office（专项）/Offer II 030. 插入、删除和随机访问都是 O(1) 的容器.java
+++ 剑指Office（专项）/Offer II 030. 插入、删除和随机访问都是 O(1) 的容器.java
@@ -10,9 +10,11 @@ class RandomizedSet {
     }
 
     public boolean insert(int val) {
+        // 判断是否存在，存在则false
         if (indices.containsKey(val)) {
             return false;
         }
+        // 不存在则获得list数量，即可记录新元素在list中的下标
         int index = nums.size();
         nums.add(val);
         indices.put(val, index);
@@ -20,9 +22,11 @@ class RandomizedSet {
     }
 
     public boolean remove(int val) {
+        // 不存在则返回false
         if (!indices.containsKey(val)) {
             return false;
         }
+        // 获取val在list的下标，用list最后一个元素覆盖，再删除最后一个元素，然后删除map中的元素。并更新最后一个元素的新位置。
         int index = indices.get(val);
         int last = nums.get(nums.size() - 1);
         nums.set(index, last);
@@ -33,6 +37,7 @@ class RandomizedSet {
     }
 
     public int getRandom() {
+        // 获取全部数量，然后随机访问。
         int randomIndex = random.nextInt(nums.size());
         return nums.get(randomIndex);
     }
