703d0c9acdfd521f46da4021c669a921e763205f
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-04-30 11:20:30 +0800

    Create Offer II 029. 排序的循环链表.java

---
+++ 剑指Office（专项）/Offer II 029. 排序的循环链表.java
@@ -0,0 +1,36 @@
+class Solution {
+    public Node insert(Node head, int insertVal) {
+        Node cur = null;
+        Node next = null;
+        Node realHead = null;
+        //空的
+        if(head == null){
+            head = new Node(insertVal);
+            head.next = head;
+            return head;
+        }
+
+        //找到真正的头节点
+        cur = head;
+        next = head.next;
+        while(cur.val <= next.val){
+            cur = cur.next;
+            next = next.next;
+            if(cur == head) break;
+        }
+        realHead = next;
+
+        //找插入位置
+        while(next.val < insertVal){
+            cur = next;
+            next = next.next;
+            //相等了，就随便插
+            if(next == realHead)break;
+        }
+        //连接
+        cur.next = new Node(insertVal);
+        cur = cur.next;
+        cur.next = next;
+        return head;
+    }
+}
