525a80e571c7ccf0330ffba578524cbeeadfd3cf
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-03 12:49:03 +0800

    Create Offer II 039. 直方图最大矩形面积.java

---
+++ 剑指Office（专项）/Offer II 039. 直方图最大矩形面积.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+        int n = heights.length;
+        int[] left = new int[n];
+        int[] right = new int[n];
+        Arrays.fill(right, n);
+
+        Deque<Integer> mono_stack = new ArrayDeque<Integer>();
+        for (int i = 0; i < n; ++i) {
+            while (!mono_stack.isEmpty() && heights[mono_stack.peek()] >= heights[i]) {
+                right[mono_stack.peek()] = i;
+                mono_stack.pop();
+            }
+            left[i] = (mono_stack.isEmpty() ? -1 : mono_stack.peek());
+            mono_stack.push(i);
+        }
+
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            ans = Math.max(ans, (right[i] - left[i] - 1) * heights[i]);
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        new Solution().largestRectangleArea(new int[]{2, 1, 5, 6, 2, 3});
+    }
+}
