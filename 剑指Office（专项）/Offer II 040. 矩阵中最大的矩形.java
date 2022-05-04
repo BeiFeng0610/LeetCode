83207f8b44a4d8b6f99943e0cfe737c0ef4d7378
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-04 23:47:18 +0800

    Update Offer II 040. 矩阵中最大的矩形.java

--- 剑指Office（专项）/Offer II 040. 矩阵中最大的矩形.java
+++ 剑指Office（专项）/Offer II 040. 矩阵中最大的矩形.java
@@ -6,7 +6,8 @@ class Solution {
         }
         int n = matrix[0].length();
         int[][] left = new int[m][n];
-
+        
+        // 初始化每一个坐标左边1的个数，即为当前行，当前下标往左的最大宽度
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
                 if (matrix[i].charAt(j) == '1') {
@@ -17,11 +18,13 @@ class Solution {
 
         int ret = 0;
         for (int i = 0; i < m; i++) {
+            // 计算每个坐标从下往上转换为直方图最大面积那一题，从下往上看成从左往右。记录最大值即可
             for (int j = 0; j < n; j++) {
                 if (matrix[i].charAt(j) == '0') {
                     continue;
                 }
                 int width = left[i][j];
+                // 当前下标，当前行左边宽度。记录面积
                 int area = width;
                 for (int k = i - 1; k >= 0; k--) {
                     width = Math.min(width, left[k][j]);
@@ -55,9 +58,11 @@ class Solution2 {
         for (int j = 0; j < n; j++) {
             int[] up = new int[m];
             int[] down = new int[m];
+            // 填充下边界全为m
             Arrays.fill(down, m);
-
+            
             Deque<Integer> stack = new LinkedList<>();
+            // 转成一列一列的直方图，记录最大值
             for (int i = 0; i < m; i++) {
                 while (!stack.isEmpty() && left[stack.peek()][j] >= left[i][j]) {
                     down[stack.peek()] = i;
