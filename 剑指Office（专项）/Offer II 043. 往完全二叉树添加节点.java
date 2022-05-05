d882cb86575e5b3b84773e0d7588719db0615dd5
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-05 23:37:19 +0800

    Update Offer II 043. 往完全二叉树添加节点.java

--- 剑指Office（专项）/Offer II 043. 往完全二叉树添加节点.java
+++ 剑指Office（专项）/Offer II 043. 往完全二叉树添加节点.java
@@ -3,7 +3,8 @@ class CBTInserter {
     TreeNode root;
     List<TreeNode> tree;
     int len = 0;
-
+    
+    // 初始化结点，统计初始树的结点数，放到list中，好随机访问
     public CBTInserter(TreeNode root) {
         this.root = root;
         tree = new ArrayList<>();
@@ -25,8 +26,10 @@ class CBTInserter {
     }
 
     public int insert(int v) {
+        // 计算父节点下标
         int par = (len - 1) / 2;
         TreeNode parNode = tree.get(par);
+        // 判断父节点左右子树
         if (parNode.left == null) {
             parNode.left = new TreeNode(v);
             tree.add(parNode.left);
