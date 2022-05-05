cb98e5345404c55d0df1b7cdeb9e0580daa95799
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-05 15:56:19 +0800

    Create Offer II 045. 二叉树最底层最左边的值.java

---
+++ 剑指Office（专项）/Offer II 045. 二叉树最底层最左边的值.java
@@ -0,0 +1,43 @@
+class Solution {
+    public int findBottomLeftValue(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        int res = root.val;
+        while (!queue.isEmpty()) {
+            res = queue.peek().val;
+            int curlen = queue.size();
+            for (int i = 0; i < curlen; i++) {
+                TreeNode tmp = queue.poll();
+                if (tmp.left != null) {
+                    queue.offer(tmp.left);
+                }
+                if (tmp.right != null) {
+                    queue.offer(tmp.right);
+                }
+            }
+        }
+        return res;
+    }
+}
+
+class Solution2 {
+    int res = 0;
+    int maxDepth = 0;
+    public int findBottomLeftValue(TreeNode root) {
+        this.res = root.val;
+        dfs(root, 0);
+        return res;
+    }
+    private void dfs(TreeNode node, int depth){
+        if(node == null) {
+            return;
+        }
+        // 表示进入新的一层，而且是最左结点
+        if(maxDepth < depth) {
+            maxDepth = depth;
+            res = node.val;
+        }
+        dfs(node.left, depth + 1);
+        dfs(node.right, depth + 1);
+    }
+}
