94a28e4a827c6939912e29c00e43c1a55a156360
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-06 22:58:59 +0800

    Update Offer II 047. 二叉树剪枝.java

--- 剑指Office（专项）/Offer II 047. 二叉树剪枝.java
+++ 剑指Office（专项）/Offer II 047. 二叉树剪枝.java
@@ -8,12 +8,14 @@ class Solution {
     }
 
     public int dfs(TreeNode node) {
+        // 空结点返回 0
         if (node == null) {
             return 0;
         }
-
+        // 记录结点值，以及子节点
         int tmp = node.val;
-
+        
+        // 子节点值为0直接置空
         if (dfs(node.left) == 0) {
             node.left = null;
         } else {
@@ -24,6 +26,7 @@ class Solution {
         } else {
             tmp += dfs(node.right);
         }
+        // 返回结点总值
         return tmp;
     }
 }
@@ -38,9 +41,11 @@ class Solution2 {
         if (node == null) {
             return null;
         }
-
+        
+        // 把左节点或者右节点置空
         node.left = dfs(node.left);
         node.right = dfs(node.right);
+        // 只有左右结点都为null且结点值为 0 时，才置空
         return node.left==null && node.right==null && node.val==0? null:node;
     }
 }
