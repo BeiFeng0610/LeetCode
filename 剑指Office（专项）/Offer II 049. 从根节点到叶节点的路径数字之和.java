e37d32e2ed50eaa540bc2e076189fbf873454cf3
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-07 12:44:06 +0800

    Create Offer II 049. 从根节点到叶节点的路径数字之和.java

---
+++ 剑指Office（专项）/Offer II 049. 从根节点到叶节点的路径数字之和.java
@@ -0,0 +1,48 @@
+class Solution {
+    public int sumNumbers(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode node, int preSum){
+        if (node == null) {
+            return 0;
+        }
+        int sum = preSum * 10 + node.val;
+        if (node.left == null && node.right == null) {
+            return sum;
+        } else {
+            return dfs(node.left, sum) + dfs(node.right, sum);
+        }
+    }
+}
+
+class Solution2 {
+    public int sumNumbers(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int sum = 0;
+        Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        Queue<Integer> numQueue = new LinkedList<Integer>();
+        nodeQueue.offer(root);
+        numQueue.offer(root.val);
+        while (!nodeQueue.isEmpty()) {
+            TreeNode node = nodeQueue.poll();
+            int num = numQueue.poll();
+            TreeNode left = node.left, right = node.right;
+            if (left == null && right == null) {
+                sum += num;
+            } else {
+                if (left != null) {
+                    nodeQueue.offer(left);
+                    numQueue.offer(num * 10 + left.val);
+                }
+                if (right != null) {
+                    nodeQueue.offer(right);
+                    numQueue.offer(num * 10 + right.val);
+                }
+            }
+        }
+        return sum;
+    }
+}
