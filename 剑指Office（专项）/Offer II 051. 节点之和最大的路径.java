91077dfa8aefd69b0f440db04962830870009988
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-07 14:19:21 +0800

    Create Offer II 051. 节点之和最大的路径.java

---
+++ 剑指Office（专项）/Offer II 051. 节点之和最大的路径.java
@@ -0,0 +1,22 @@
+class Solution {
+    int maxSum = Integer.MIN_VALUE;
+
+    public int maxPathSum(TreeNode root) {
+        maxGain(root);
+        return maxSum;
+    }
+
+    public int maxGain(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+
+        int leftGain = Math.max(maxGain(node.left), 0);
+        int rightGain = Math.max(maxGain(node.right), 0);
+
+        int priceNewpath = node.val + leftGain + rightGain;
+
+        maxSum = Math.max(maxSum, priceNewpath);
+        return node.val + Math.max(leftGain, rightGain);
+    }
+}
