022abaa4088d8a73ea7c51f3b67dbb5699b15b72
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-07 22:42:21 +0800

    Update Offer II 049. 从根节点到叶节点的路径数字之和.java

--- 剑指Office（专项）/Offer II 049. 从根节点到叶节点的路径数字之和.java
+++ 剑指Office（专项）/Offer II 049. 从根节点到叶节点的路径数字之和.java
@@ -7,10 +7,13 @@ class Solution {
         if (node == null) {
             return 0;
         }
+        // 得出前一个结点和当前结点的组合值
         int sum = preSum * 10 + node.val;
+        // 如果当前结点为叶子结点，则返回值进行累加
         if (node.left == null && node.right == null) {
             return sum;
         } else {
+            // 如果不是叶子结点，就去获取子节点的值。
             return dfs(node.left, sum) + dfs(node.right, sum);
         }
     }
@@ -22,17 +25,22 @@ class Solution2 {
             return 0;
         }
         int sum = 0;
+        // 结点队列
         Queue<TreeNode> nodeQueue = new LinkedList<TreeNode>();
+        // 值队列
         Queue<Integer> numQueue = new LinkedList<Integer>();
         nodeQueue.offer(root);
         numQueue.offer(root.val);
         while (!nodeQueue.isEmpty()) {
+            // 弹出队头，获取值
             TreeNode node = nodeQueue.poll();
             int num = numQueue.poll();
             TreeNode left = node.left, right = node.right;
+            // 如果是叶子结点，就累加。
             if (left == null && right == null) {
                 sum += num;
             } else {
+                // 不是叶子结点，就把子节点加入队列，把组合值加入值队列
                 if (left != null) {
                     nodeQueue.offer(left);
                     numQueue.offer(num * 10 + left.val);
