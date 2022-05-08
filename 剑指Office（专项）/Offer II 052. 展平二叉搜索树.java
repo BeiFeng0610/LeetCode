5283049c2c340af99e1af646f797a5d489317b2d
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-08 23:09:19 +0800

    Update Offer II 052. 展平二叉搜索树.java

--- 剑指Office（专项）/Offer II 052. 展平二叉搜索树.java
+++ 剑指Office（专项）/Offer II 052. 展平二叉搜索树.java
@@ -1,6 +1,7 @@
 class Solution {
     TreeNode pre = null;
     public TreeNode increasingBST(TreeNode root) {
+        // 创建一个前置结点
         TreeNode res = new TreeNode(-1);
         pre = res;
         dfs(root);
@@ -12,8 +13,10 @@ class Solution {
             return;
         }
         dfs(node.left);
+        // 前置结点的右子树指向当前结点，当前结点，左子树置空。
         pre.right = node;
         node.left = null;
+        // 更新前置结点
         pre = node;
         dfs(node.right);
     }
