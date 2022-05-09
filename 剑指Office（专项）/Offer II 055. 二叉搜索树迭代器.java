5926ca3b6d43178a39066c0bcfce4f0123fcd862
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-09 23:17:42 +0800

    Update Offer II 055. 二叉搜索树迭代器.java

--- 剑指Office（专项）/Offer II 055. 二叉搜索树迭代器.java
+++ 剑指Office（专项）/Offer II 055. 二叉搜索树迭代器.java
@@ -2,10 +2,10 @@ class BSTIterator {
     Queue<TreeNode> queue;
     TreeNode root = null;
 
-
     public BSTIterator(TreeNode root) {
         queue = new LinkedList<>();
         this.root = root;
+        // 把所有结点都加入队列
         dfs(this.root);
     }
 
@@ -20,6 +20,7 @@ class BSTIterator {
         return queue.size() > 0;
     }
 
+    // 中序遍历加入队列
     private void dfs(TreeNode node) {
         if (node == null) {
             return;
@@ -40,12 +41,15 @@ class BSTIterator2 {
     }
 
     public int next() {
+        // 当前结点不为null，就压栈，直到最左下结点。
         while (cur != null) {
             stack.push(cur);
             cur = cur.left;
         }
+        // 然后弹出最左下结点。
         cur = stack.pop();
         int ret = cur.val;
+        // 然后当前结点为右结点，如果右结点为空，则弹出父节点。
         cur = cur.right;
         return ret;
     }
