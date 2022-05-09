8480c75f9b127f0c4957ec34d3d934104ebcc83c
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-09 23:51:03 +0800

    Update Offer II 057. 值和下标之差都在给定的范围内.java

--- 剑指Office（专项）/Offer II 057. 值和下标之差都在给定的范围内.java
+++ 剑指Office（专项）/Offer II 057. 值和下标之差都在给定的范围内.java
@@ -25,9 +25,12 @@ class Solution2 {
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
         int n = nums.length;
         Map<Long, Long> map = new HashMap<Long, Long>();
+        // 桶排序，差值，一个桶内的肯定满足，邻桶可能满足
         long w = (long) t + 1;
         for (int i = 0; i < n; i++) {
+            //获取桶号
             long id = getID(nums[i], w);
+            // 存在直接返回 不存在判断邻桶
             if (map.containsKey(id)) {
                 return true;
             }
@@ -37,6 +40,7 @@ class Solution2 {
             if (map.containsKey(id + 1) && Math.abs(nums[i] - map.get(id + 1)) < w) {
                 return true;
             }
+            // 保持窗口宽度
             map.put(id, (long) nums[i]);
             if (i >= k) {
                 map.remove(getID(nums[i - k], w));
@@ -46,6 +50,7 @@ class Solution2 {
     }
 
     public long getID(long x, long w) {
+        //自然数桶编号，负数因为不包含0号桶也不包含0，所以x+1  桶序号-1
         if (x >= 0) {
             return x / w;
         }
