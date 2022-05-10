8ff43e4ec0c39451f64370c808d00ee9f5597e8e
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-10 23:44:42 +0800

    Update Offer II 059. 数据流的第 K 大数值.java

--- 剑指Office（专项）/Offer II 059. 数据流的第 K 大数值.java
+++ 剑指Office（专项）/Offer II 059. 数据流的第 K 大数值.java
@@ -5,6 +5,7 @@ class KthLargest {
 
     public KthLargest(int k, int[] nums) {
         this.k = k;
+        // 依次把元素放进优先队列
         queue = new PriorityQueue<>();
         for (int n : nums) {
             queue.offer(n);
@@ -12,6 +13,7 @@ class KthLargest {
     }
 
     public int add(int val) {
+        // 添加元素，把元素弹出到剩余k个元素，然后弹出第一个元素即可
         queue.offer(val);
         while (queue.size() > k) {
             queue.poll();
