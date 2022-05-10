7229f30a9a56a8bfe260428fcbf644c1771a32c3
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-10 23:37:19 +0800

    Update Offer II 058. 日程表.java

--- 剑指Office（专项）/Offer II 058. 日程表.java
+++ 剑指Office（专项）/Offer II 058. 日程表.java
@@ -6,9 +6,11 @@ class MyCalendar {
     }
 
     public boolean book(int start, int end) {
-
+        // 使用treeMap，ceilingEntry获取key >= start 中最小的一个
         Map.Entry<Integer, Integer> ceilingEntry = treeMap.ceilingEntry(start);
+        // 使用treeMap，floorEntry获取key <= start 中最大的一个
         Map.Entry<Integer, Integer> floorEntry = treeMap.floorEntry(start);
+        // 如果没有比start更小的，就判断end <= ceilingEntry
         if ((floorEntry == null || floorEntry.getValue() <= start)
                 && (ceilingEntry == null || end <= ceilingEntry.getKey())) {
             treeMap.put(start, end);
