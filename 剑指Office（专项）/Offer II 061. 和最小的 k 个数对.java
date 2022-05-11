68d74242efb943498243f5fae529bb3c16f94beb
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-11 23:19:54 +0800

    Update Offer II 061. 和最小的 k 个数对.java

--- 剑指Office（专项）/Offer II 061. 和最小的 k 个数对.java
+++ 剑指Office（专项）/Offer II 061. 和最小的 k 个数对.java
@@ -1,16 +1,17 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-
+        // 暴力法
         PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return (o1[0] + o1[1]) - (o2[0] + o2[1]);
             }
         });
-        PriorityQueue<int[]> minHeap = new PriorityQueue<>((a,b) -> nums1[a[0]] + nums2[a[1]] - nums1[b[0]] - nums2[b[1]]);
+        // 取边界，和最大组合
         int len1 = Math.min(nums1.length, k);
         int len2 = Math.min(nums2.length, k);
         int r = Math.min(len1 * len2, k);
+        // 暴力枚举
         for (int i = 0; i < len1; i++) {
             for (int j = 0; j < len2; j++) {
                 queue.offer(new int[]{nums1[i], nums2[j]});
@@ -32,20 +33,25 @@ class Solution {
 class Solution2 {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
         PriorityQueue<int[]> pq = new PriorityQueue<>(k, (o1, o2)->{
+            // 根据下标组合排序
             return nums1[o1[0]] + nums2[o1[1]] - nums1[o2[0]] - nums2[o2[1]];
         });
         List<List<Integer>> ans = new ArrayList<>();
         int m = nums1.length;
         int n = nums2.length;
+        // 初始化第一个下标
         for (int i = 0; i < Math.min(m, k); i++) {
             pq.offer(new int[]{i,0});
         }
+        // 堆顶不空，最多循环k次。
         while (k-- > 0 && !pq.isEmpty()) {
+            // 获取堆顶
             int[] idxPair = pq.poll();
             List<Integer> list = new ArrayList<>();
             list.add(nums1[idxPair[0]]);
             list.add(nums2[idxPair[1]]);
             ans.add(list);
+            // 判断第二下标边。
             if (idxPair[1] + 1 < n) {
                 pq.offer(new int[]{idxPair[0], idxPair[1] + 1});
             }
