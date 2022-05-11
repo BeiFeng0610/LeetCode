682869029ffbff617a87d2ff9f1001cab78e182a
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-11 23:31:30 +0800

    Update Offer II 063. 替换单词.java

--- 剑指Office（专项）/Offer II 063. 替换单词.java
+++ 剑指Office（专项）/Offer II 063. 替换单词.java
@@ -3,6 +3,7 @@ class Solution {
         String[] strings = sentence.split(" ");
 
         for (String pre : dictionary) {
+            // 暴力枚举，startsWith判断是否包含，然后替换。
             for (int i = 0; i < strings.length; i++) {
                 if (strings[i].startsWith(pre)) {
                     strings[i] = pre;
@@ -25,17 +26,19 @@ class Solution2 {
         }
     }
 
+    // 字典树根节点。
     TrieNode root = new TrieNode();
     public String replaceWords(List<String> dictionary, String sentence) {
+        // 获取所有前缀
         String[] words = new String[dictionary.size()];
         for (int i = 0; i < words.length; i++) {
             words[i] = dictionary.get(i);
         }
-
+        // 全部加入字典树
         for (String word : words) {
             insert(root, word);
         }
-
+        
         String[] strs = sentence.split(" ");
         for (int i = 0; i < strs.length; i++) {
             if (search(root, strs[i])) {
@@ -43,6 +46,7 @@ class Solution2 {
             }
         }
 
+        // 拼接所有字符串
         StringBuilder sb = new StringBuilder();
         for(String s : strs){
             sb.append(s).append(" ");
@@ -65,9 +69,11 @@ class Solution2 {
     public boolean search(TrieNode root, String s){
         TrieNode node = root;
         for(char ch : s.toCharArray()){
+            // 包含词根直接返回
             if(node.isValid) {
                 break;
             }
+            // 单词不包含词根
             if(node.kids[ch - 'a'] == null) {
                 return false;
             }
@@ -80,10 +86,12 @@ class Solution2 {
         TrieNode node = root;
         StringBuilder sb = new StringBuilder();
         for(char ch : s.toCharArray()){
+            // 包含词根，或者遍历结束不包含，就返回
             if(node.isValid || node.kids[ch - 'a'] == null) {
                 break;
             }
             node = node.kids[ch - 'a'];
+            // 拼接当前字母
             sb.append(ch);
         }
         return sb.toString();
