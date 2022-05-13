caee1126e8032bb84cfaee48b4a64a22da1a59ef
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-13 23:22:27 +0800

    Update Offer II 067. 最大的异或.java

--- 剑指Office（专项）/Offer II 067. 最大的异或.java
+++ 剑指Office（专项）/Offer II 067. 最大的异或.java
@@ -4,9 +4,11 @@ class Solution {
 
     public int findMaximumXOR(int[] nums) {
         int x = 0;
+        // 从高位开始统计
         for (int k = HIGH_BIT; k >= 0; --k) {
             Set<Integer> seen = new HashSet<Integer>();
             // 将所有的 pre^k(a_j) 放入哈希表中
+            // 如果所有的数中高位有 1 就记录到set中
             for (int num : nums) {
                 // 如果只想保留从最高位开始到第 k 个二进制位为止的部分
                 // 只需将其右移 k 位
@@ -15,10 +17,15 @@ class Solution {
 
             // 目前 x 包含从最高位开始到第 k+1 个二进制位为止的部分
             // 我们将 x 的第 k 个二进制位置为 1，即为 x = x*2+1
+            // 设当前位可以被添加到 xNext 中例如，统计了1 然后下一位是，设11成立
             int xNext = x * 2 + 1;
             boolean found = false;
 
             // 枚举 i
+            // 如果设的 xNext 异或 (num >> k)在set中存在，说明 x 必定存在。
+            // 注意可能位第k位，次数高位可能有好几个1，所有计算异或的时候是连带高位一起运算。
+            // 就是可能有最高位那个数（也可能有多个） 和 另一个数在第一次满足运算（也可能有多个数满足）
+            // 第二次运算其实就是从他们中间开始第二位模拟，筛选出来之后，继续从子集中模拟
             for (int num : nums) {
                 if (seen.contains(xNext ^ (num >> k))) {
                     found = true;
@@ -26,11 +33,13 @@ class Solution {
                 }
             }
 
+            // 如果找到了，就更新设的xNext到x中
             if (found) {
                 x = xNext;
             } else {
                 // 如果没有找到满足等式的 a_i 和 a_j，那么 x 的第 k 个二进制位只能为 0
                 // 即为 x = x*2
+                // 其实就是不满足，进行下一次摸你。高位左移一位。
                 x = xNext - 1;
             }
         }
@@ -51,6 +60,7 @@ class Solution2 {
             // 将 nums[i-1] 放入字典树，此时 nums[0 .. i-1] 都在字典树中
             add(nums[i - 1]);
             // 将 nums[i] 看作 ai，找出最大的 x 更新答案
+            // 每一次check，对比就更新最大值。
             x = Math.max(x, check(nums[i]));
         }
         return x;
@@ -58,6 +68,8 @@ class Solution2 {
 
     public void add(int num) {
         Trie cur = root;
+        // 将num添加到字典树中，二进制位0就创建走左子树，不存在就新建。
+        // 而且从高位开始，位数不满足全部补0
         for (int k = HIGH_BIT; k >= 0; --k) {
             int bit = (num >> k) & 1;
             if (bit == 0) {
@@ -81,6 +93,7 @@ class Solution2 {
             int bit = (num >> k) & 1;
             if (bit == 0) {
                 // a_i 的第 k 个二进制位为 0，应当往表示 1 的子节点 right 走
+                // 因为异或0走1，1不存在就走0，x左移一位（因为没有满足的位）。
                 if (cur.right != null) {
                     cur = cur.right;
                     x = x * 2 + 1;
@@ -90,6 +103,7 @@ class Solution2 {
                 }
             } else {
                 // a_i 的第 k 个二进制位为 1，应当往表示 0 的子节点 left 走
+                // 因为异或1走0，0不存在就走1，x左移一位（因为没有满足的位）。
                 if (cur.left != null) {
                     cur = cur.left;
                     x = x * 2 + 1;
@@ -99,6 +113,7 @@ class Solution2 {
                 }
             }
         }
+        // 最后返回结果值即可
         return x;
     }
 }
