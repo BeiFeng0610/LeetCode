b5a4a36c2fe416b91eaa13dafd8e8be44a102cf3
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-13 23:27:12 +0800

    Update Offer II 068. 查找插入位置.java

--- 剑指Office（专项）/Offer II 068. 查找插入位置.java
+++ 剑指Office（专项）/Offer II 068. 查找插入位置.java
@@ -4,8 +4,10 @@ class Solution {
         int l = 0, r = len - 1;
         while (l <= r) {
             int mid = (r - l) / 2 + l;
+            // 左右相等 mid 还小于target，就应该左边界右移，这是插入的位置
             if (nums[mid] < target) {
                 l = mid + 1;
+                // 大于或者等于 右边界 左移，l指向的是找的的结点，也可以是可插入结点，因为l左边都是小于l的
             } else {
                 r = mid - 1;
             }
