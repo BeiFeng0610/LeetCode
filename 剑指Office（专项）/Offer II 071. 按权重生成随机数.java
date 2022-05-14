0c8dfbd88e0c76379df0cbb981981f9ce0448414
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-14 23:50:29 +0800

    Update Offer II 071. 按权重生成随机数.java

--- 剑指Office（专项）/Offer II 071. 按权重生成随机数.java
+++ 剑指Office（专项）/Offer II 071. 按权重生成随机数.java
@@ -5,14 +5,18 @@ class Solution {
         pre = new int[w.length];
         pre[0] = w[0];
         total = w[0];
+        // 创建新数组，统计前缀和，也就是下标触发的权值区间
         for (int i = 1; i < w.length; i++) {
             pre[i] = pre[i - 1] + w[i];
+            // 统计权值
             total += w[i];
         }
     }
 
     public int pickIndex() {
+        // 权值从1开始，需要随机数+1
         int idx = (int) (Math.random() * total) + 1;
+        // 找到权值所在的区间，也就是数组可插入的区间(]
         return binarySearch(idx);
     }
     private int binarySearch(int x) {
