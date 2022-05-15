4608c682bbe80cc19859849baccb72fe1f3f1768
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-15 23:10:23 +0800

    Update Offer II 073. 狒狒吃香蕉.java

--- 剑指Office（专项）/Offer II 073. 狒狒吃香蕉.java
+++ 剑指Office（专项）/Offer II 073. 狒狒吃香蕉.java
@@ -4,11 +4,14 @@ class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         this.piles = piles;
         int max = 0;
+        // 求最大的香蕉堆
         for (int p : piles) {
             max = Math.max(p, max);
         }
+        // 最大堆为，最大速度
         int l = 1, r = max;
         while (l < r) {
+            // 求中间速度，然后对比所需时间
             int mid = (r - l) / 2 + l;
             if (getHour(mid) > h) {
                 l = mid + 1;
@@ -19,7 +22,7 @@ class Solution {
         return l;
     }
 
-
+    // 传入速度，计算所需时间
     private int getHour(int speed){
         int hour = 0;
         for (int p : piles) {
