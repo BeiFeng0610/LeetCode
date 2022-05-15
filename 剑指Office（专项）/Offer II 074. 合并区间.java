bc3e1c5481b6257c1bdf2cea1a797e53a6d019ca
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-15 14:00:38 +0800

    Update Offer II 074. 合并区间.java

--- 剑指Office（专项）/Offer II 074. 合并区间.java
+++ 剑指Office（专项）/Offer II 074. 合并区间.java
@@ -1,21 +1,18 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        if (intervals.length == 0) {
-            return new int[0][2];
+        int len = intervals.length;
+        if (len == 1) {
+            return intervals;
         }
-        Arrays.sort(intervals, new Comparator<int[]>() {
-            @Override
-            public int compare(int[] interval1, int[] interval2) {
-                return interval1[0] - interval2[0];
-            }
-        });
-        List<int[]> merged = new ArrayList<int[]>();
-        for (int i = 0; i < intervals.length; ++i) {
-            int L = intervals[i][0], R = intervals[i][1];
-            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < L) {
-                merged.add(new int[]{L, R});
+        Arrays.sort(intervals, (o1, o2) -> o1[0] - o2[0]);
+        List<int[]> merged = new ArrayList<>();
+
+        for (int i = 0; i < len; i++) {
+            int l = intervals[i][0], r = intervals[i][1];
+            if (merged.size() == 0 || merged.get(merged.size() - 1)[1] < l) {
+                merged.add(new int[]{l, r});
             } else {
-                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], R);
+                merged.get(merged.size() - 1)[1] = Math.max((merged.get(merged.size() - 1)[1]), r);
             }
         }
         return merged.toArray(new int[merged.size()][]);
