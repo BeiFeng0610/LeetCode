c5586f0db4de39ab10056dbd832976b822497f4e
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-17 23:09:59 +0800

    Update Offer II 080. 含有 k 个元素的组合.java

--- 剑指Office（专项）/Offer II 080. 含有 k 个元素的组合.java
+++ 剑指Office（专项）/Offer II 080. 含有 k 个元素的组合.java
@@ -10,6 +10,7 @@ class Solution {
         return res;
     }
     public void dfs(int n, int k) {
+        // 提前剪枝，tmp中元素  加上后面所有的还是小于 k，就肯定不满足，直接返回
         if ((tmp.size() + (d - n + 1)) < k) {
             return;
         }
@@ -17,9 +18,13 @@ class Solution {
             res.add(new ArrayList<>(tmp));
             return;
         }
+        // 添加当前层
         tmp.add(n);
+        // 进入下一层
         dfs(n + 1, k);
+        // 删除当前层
         tmp.remove(tmp.size() - 1);
+        // 继续下一层
         dfs(n + 1, k);
     }
     
