83512eb8a517f12a5013909dd73ea759dd670aab
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-17 23:07:47 +0800

    Update Offer II 079. 所有子集.java

--- 剑指Office（专项）/Offer II 079. 所有子集.java
+++ 剑指Office（专项）/Offer II 079. 所有子集.java
@@ -2,8 +2,10 @@ class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
         int n = nums.length;
+        // 根据数组长度，定义一个宽度为n的二进制数 比如n=3 二进制位111
         for (int i = 0; i < (1 << n); i++) {
             List<Integer> tmp = new ArrayList<>();
+            // 所有子集即为：001 010 011 100 101 110 111 刚好符合二进制
             for (int j = 0; j < n; j++) {
                 if ((i & (1 << j)) != 0) {
                     tmp.add(nums[j]);
@@ -25,12 +27,16 @@ class Solution2 {
 
     public void dfs(int cur, int[] nums) {
         if (cur == nums.length) {
+            // 到尾部就添加
             ans.add(new ArrayList<>(t));
             return;
         }
+        // 全部添加，直到加满
         t.add(nums[cur]);
         dfs(cur + 1, nums);
+        // 删除集合最后一个结点，也就是删除当前层，找下面的层
         t.remove(t.size() - 1);
+        // 继续添加
         dfs(cur + 1, nums);
     }
 }
