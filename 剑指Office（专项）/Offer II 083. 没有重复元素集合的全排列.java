b230f886936965a165bbe5a7fb68256530523a17
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-18 22:41:03 +0800

    Update Offer II 083. 没有重复元素集合的全排列.java

--- 剑指Office（专项）/Offer II 083. 没有重复元素集合的全排列.java
+++ 剑指Office（专项）/Offer II 083. 没有重复元素集合的全排列.java
@@ -2,6 +2,7 @@ class Solution {
     List<List<Integer>> res = new ArrayList<>();
     List<Integer> output = new ArrayList<>();
     public List<List<Integer>> permute(int[] nums) {
+        // 把所有数放到集合中
         for (int num : nums) {
             output.add(num);
         }
@@ -11,12 +12,17 @@ class Solution {
     }
 
     public void backtrack(int n, int first) {
+        // 确定数字个数==n是，说明已经全部确定
         if (first == n) {
+            // 把当前状态的集合加入
             res.add(new ArrayList<>(output));
         }
+        // i 表示第几个数，依次把 1 2 ... n作为头，然后确定第二个，从集合中的第二个数开始，以此类推。
         for (int i = first; i < n; i++) {
+            // first表示需要确定的位数，交换即可，可交换数的区间为[first,n]
             Collections.swap(output, first, i);
             backtrack(n, first + 1);
+            // 交换回来，恢复原集合
             Collections.swap(output, first, i);
         }
 
