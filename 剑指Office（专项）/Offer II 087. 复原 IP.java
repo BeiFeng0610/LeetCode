2dbf244d9417a91d2f3378cd9fd4dc3f761c88c5
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-20 11:31:14 +0800

    Update Offer II 087. 复原 IP.java

--- 剑指Office（专项）/Offer II 087. 复原 IP.java
+++ 剑指Office（专项）/Offer II 087. 复原 IP.java
@@ -1,8 +1,9 @@
 class Solution {
+    // IP 最多有四部分组成
     static final int SEG_COUNT = 4;
     List<String> ans = new ArrayList<>();
     int[] segments = new int[SEG_COUNT];
-
+    
     public List<String> restoreIpAddresses(String s) {
         segments = new int[SEG_COUNT];
         dfs(s, 0, 0);
@@ -10,7 +11,9 @@ class Solution {
     }
 
     public void dfs(String s,int segId,int segStart) {
+        // 字段满足4
         if (segId == SEG_COUNT) {
+            // 起始等于长度表示全部添加了，否则返回，不满足
             if (segStart == s.length()) {
                 StringBuilder ipAddr = new StringBuilder();
                 for (int i = 0; i < SEG_COUNT; i++) {
@@ -23,11 +26,11 @@ class Solution {
             }
             return;
         }
-
+        // 如果起始 等于 字符串长度，就返回
         if (segStart == s.length()) {
             return;
         }
-
+        // 前导0，这数字段只能为0，不能0+数字
         if (s.charAt(segStart) == '0') {
             segments[segId] = 0;
             dfs(s, segId + 1, segStart + 1);
@@ -35,6 +38,7 @@ class Solution {
 
         int addr = 0;
         for (int segEnd = segStart; segEnd < s.length(); ++segEnd) {
+            // 累加 判断 addr 大于0 且小于等于 255，然后添加
             addr = addr * 10 + (s.charAt(segEnd) - '0');
             if (addr > 0 && addr <= 0xFF) {
                 segments[segId] = addr;
