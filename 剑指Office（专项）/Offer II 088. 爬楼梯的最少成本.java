c6b0887e37fd7f1954233b6c51a94be57c892765
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-21 16:38:05 +0800

    Update Offer II 088. 爬楼梯的最少成本.java

--- 剑指Office（专项）/Offer II 088. 爬楼梯的最少成本.java
+++ 剑指Office（专项）/Offer II 088. 爬楼梯的最少成本.java
@@ -3,6 +3,7 @@ class Solution {
         int[] dp = new int[cost.length + 1];
         dp[0] = cost[0];
         dp[1] = cost[1];
+        // 动态规划，找到两个前置结点，计算哪个最小即可
         for (int i = 2; i <= cost.length; i++) {
             dp[i] = Math.min(dp[i - 1], dp[i - 2]) + (cost.length == i ? 0 : cost[i]);
         }
