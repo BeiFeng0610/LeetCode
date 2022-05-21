d8d135c220dc34c3e6e822890b6f8092810a0ed2
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-21 18:40:02 +0800

    Update Offer II 089. 房屋偷盗.java

--- 剑指Office（专项）/Offer II 089. 房屋偷盗.java
+++ 剑指Office（专项）/Offer II 089. 房屋偷盗.java
@@ -3,13 +3,13 @@ class Solution {
         int[] dp = new int[nums.length];
         int pre = 0;
         int cur = 0;
-
+        // 也是找到两个结点，判断中间可两边那个大即可
         for (int i = 0; i < nums.length; i++) {
             dp[i] = Math.max(cur, pre + nums[i]);
             pre = cur;
             cur = dp[i];
         }
-
+        
         return dp[nums.length - 1];
     }
 }
