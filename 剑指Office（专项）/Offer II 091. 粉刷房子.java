f800e01b1c19785c1c193e90c8cb7abd6ec14183
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-22 21:55:18 +0800

    Update Offer II 091. 粉刷房子.java

--- 剑指Office（专项）/Offer II 091. 粉刷房子.java
+++ 剑指Office（专项）/Offer II 091. 粉刷房子.java
@@ -3,11 +3,12 @@ class Solution {
         int red = costs[0][0];
         int green = costs[0][1];
         int blue = costs[0][2];
-
+        // 初始化三种颜色，然后依次计算不相邻的那个最小。  
         for (int i = 1; i < costs.length; i++) {
             int redNext = costs[i][0] + Math.min(green, blue);
             int greenNext = costs[i][1] + Math.min(red, blue);
             int blueNext = costs[i][2] + Math.min(green, red);
+            // 更新新一层
             red = redNext;
             green = greenNext;
             blue = blueNext;
