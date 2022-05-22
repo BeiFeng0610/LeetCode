dcdbd5c4fb55e0c0794ed7dcd7b7087b2a8fe5c3
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-22 22:49:49 +0800

    Update Offer II 092. 翻转字符.java

--- 剑指Office（专项）/Offer II 092. 翻转字符.java
+++ 剑指Office（专项）/Offer II 092. 翻转字符.java
@@ -4,6 +4,12 @@ class Solution {
         int one = 0, zero = 0;
         for(char c : s.toCharArray()){
             if(c == '0') {
+                // 如果为0，并且前面有1，就抵消一个，抵消完毕后，就不继续加入
+                // 例如：1100，抵消两个，记录2
+                // 11000，只需要抵消两个，记录2
+                // 110，抵消一个，记录1
+                // 01101000，总共抵消三个，记录3
+                // 0100110011000，总共抵消5个
                 one = Math.min(zero, one) + 1;
             } else if(c == '1'){
                 one = Math.min(zero, one);
