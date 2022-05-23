fae62446fad3f0dea8fb089894a17f5fb7c795ed
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-23 23:37:15 +0800

    Update Offer II 099. 最小路径之和.java

--- 剑指Office（专项）/Offer II 099. 最小路径之和.java
+++ 剑指Office（专项）/Offer II 099. 最小路径之和.java
@@ -3,20 +3,19 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
         int[][] dp = new int[m+1][n+1];
+        // 初始化第一行第一列，因为只有一条路
         for (int i = 1; i <= m; i++) {
             dp[i][1] = dp[i-1][1] + grid[i-1][0];
         }
         for (int i = 1; i <= n; i++) {
             dp[1][i] = dp[1][i - 1] + grid[0][i - 1];
         }
-
+        // 用当前结点，加上左上数值小的结点
         for (int i = 2; i <= m; i++) {
             for (int j = 2; j <= n; j++) {
                 dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
             }
         }
-
         return dp[m][n];
-
     }
 }
