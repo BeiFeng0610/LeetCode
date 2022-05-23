a8f7b0d7dda19289c496624345a9cd2dbd243c97
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-23 23:31:32 +0800

    Update Offer II 098. 路径的数目.java

--- 剑指Office（专项）/Offer II 098. 路径的数目.java
+++ 剑指Office（专项）/Offer II 098. 路径的数目.java
@@ -4,9 +4,11 @@ class Solution {
         for (int i = 0; i < m; i++) {
             dp[i][0] = 1;
         }
+        // 初始化第一行一列。
         for (int i = 0; i < n; i++) {
             dp[0][i] = 1;
         }
+        // 只能往右和下走，所以只用计算，左上路径的总数即可。
         for (int i = 1; i < m; i++) {
             for (int j = 1; j < n; j++) {
                 dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
