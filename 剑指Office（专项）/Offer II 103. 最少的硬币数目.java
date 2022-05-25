d9998d399ece75d796eb6ca9bf2032526edcf119
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-25 22:41:21 +0800

    Update Offer II 103. 最少的硬币数目.java

--- 剑指Office（专项）/Offer II 103. 最少的硬币数目.java
+++ 剑指Office（专项）/Offer II 103. 最少的硬币数目.java
@@ -6,22 +6,27 @@ class Solution {
         return coinChange(coins, amount, new int[amount]);
     }
     private int coinChange(int[] coins, int rem, int[] count) {
+        // 小于零说明，硬币多了，不符合
         if (rem < 0) {
             return -1;
         }
+        // 等于零则返回0
         if (rem == 0) {
             return 0;
         }
+        // 如果count最后有值则说明有其他情况
         if (count[rem - 1] != 0) {
             return count[rem - 1];
         }
         int min = Integer.MAX_VALUE;
         for (int coin : coins) {
+            // 遍历所有组合
             int res = coinChange(coins, rem - coin, count);
             if (res >= 0 && res < min) {
                 min = 1 + res;
             }
         }
+        // 更新最小的组合
         count[rem - 1] = (min == Integer.MAX_VALUE ? -1 : min);
         return count[rem - 1];
     }
