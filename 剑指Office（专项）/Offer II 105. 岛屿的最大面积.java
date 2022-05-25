c3c40b8f77b0a2e0c7c568c2de8004f64432c368
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-25 14:40:48 +0800

    Update Offer II 105. 岛屿的最大面积.java

--- 剑指Office（专项）/Offer II 105. 岛屿的最大面积.java
+++ 剑指Office（专项）/Offer II 105. 岛屿的最大面积.java
@@ -16,7 +16,6 @@ class Solution {
         }
         return max;
     }
-
     public int dfs(int[][] grid, int i, int j) {
         if (i < 0 || j < 0 || grid[i][j] == 0) {
             return 0;
@@ -24,21 +23,49 @@ class Solution {
         tag[i][j] = true;
         int top = 0, bottom = 0, left = 0, right = 0;
         if (i > 0 && !tag[i - 1][j]) {
-            tag[i - 1][j] = true;
             top = dfs(grid, i - 1, j);
         }
         if (i < grid.length - 1 && !tag[i + 1][j]) {
-            tag[i + 1][j] = true;
             bottom = dfs(grid, i + 1, j);
         }
         if (j > 0 && !tag[i][j-1]) {
-            tag[i][j - 1] = true;
             left = dfs(grid, i, j - 1);
         }
         if (j < grid[0].length-1 && !tag[i][j + 1]) {
-            tag[i][j + 1] = true;
             right = dfs(grid, i, j + 1);
         }
         return top + bottom + left + right + 1;
+    }
+}
+
+// 精简版道理相同
+class Solution {
+    int max = 0, cur = 0;
+    public int maxAreaOfIsland(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (grid[i][j] == 1) {
+                    cur = 0;
+                    dfs(grid, i, j);
+                    max = Math.max(max, cur);
+                }
+            }
+        }
+        return max;
+    }
+    public void dfs(int[][] grid, int i, int j) {
+        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] == 0) {
+            return;
+        }
+        if (grid[i][j] == 1) {
+            cur++;
+            grid[i][j]=0;
+        }
+        dfs(grid, i - 1, j);
+        dfs(grid, i + 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
     }
 }
