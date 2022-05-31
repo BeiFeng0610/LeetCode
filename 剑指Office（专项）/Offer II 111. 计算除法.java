31d9a0bca3dc5f46528f9766ca201541c647ddb9
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-31 20:01:45 +0800

    Update Offer II 111. 计算除法.java

--- 剑指Office（专项）/Offer II 111. 计算除法.java
+++ 剑指Office（专项）/Offer II 111. 计算除法.java
@@ -1,6 +1,7 @@
 class Solution {
     public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
         int nvars = 0;
+        // 把所有的字符串放入到map中。
         Map<String, Integer> variables = new HashMap<>();
         int n = equations.size();
         for (int i = 0; i < n; i++) {
@@ -12,6 +13,7 @@ class Solution {
             }
         }
 
+        // 列举每个 字符串 所相邻的字符串的比值。
         List<Pair>[] edges = new List[nvars];
         for (int i = 0; i < nvars; i++) {
             edges[i] = new ArrayList<>();
@@ -21,11 +23,14 @@ class Solution {
             edges[va].add(new Pair(vb, values[i]));
             edges[vb].add(new Pair(va, 1.0 / values[i]));
         }
+        
+        // 遍历每个问题
         int queriesCount = queries.size();
         double[] ret = new double[queriesCount];
         for (int i = 0; i < queriesCount; i++) {
             List<String> query = queries.get(i);
             double result = -1.0;
+            // 如果问题的每个字符串都存在则，找到相应的比值。
             if (variables.containsKey(query.get(0)) && variables.containsKey(query.get(1))) {
                 int ia = variables.get((query.get(0))), ib = variables.get(query.get(1));
                 if (ia == ib) {
@@ -36,12 +41,13 @@ class Solution {
                     double[] ratios = new double[nvars];
                     Arrays.fill(ratios, -1.0);
                     ratios[ia] = 1.0;
-
+                    
                     while (!points.isEmpty() && ratios[ib] < 0) {
                         int x = points.poll();
                         for (Pair pair : edges[x]) {
                             int y = pair.index;
                             double val = pair.value;
+                            // 如果该字符串有路径相连，但没有更新比值，则更新。
                             if (ratios[y] < 0) {
                                 ratios[y] = ratios[x] * val;
                                 points.offer(y);
