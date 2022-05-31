27b8adaf8ba1f8c8ebd2d4d17078ca92edaf1a44
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-05-31 19:56:35 +0800

    Update Offer II 110. 所有路径.java

--- 剑指Office（专项）/Offer II 110. 所有路径.java
+++ 剑指Office（专项）/Offer II 110. 所有路径.java
@@ -9,13 +9,16 @@ class Solution {
         return ret;
     }
 
+    // 传入图，以及当前所在结点
     public void dfs(int[][] graph, int n) {
         list.add(n);
+        // 结点满足 n-1 则返回
         if (n == graph.length - 1) {
             ret.add(new ArrayList<>(list));
             list.remove(list.size() - 1);
             return;
         }
+        // 遍历每层结点相邻的结点。
         for (int i = 0; i < graph[n].length; i++) {
             dfs(graph, graph[n][i]);
         }
