e06c1856910c82ac453438b34ce6bc0729e431b4
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-02 13:04:36 +0800

    Update Offer II 114. 外星文字典.java

--- 剑指Office（专项）/Offer II 114. 外星文字典.java
+++ 剑指Office（专项）/Offer II 114. 外星文字典.java
@@ -1,4 +1,5 @@
 class Solution {
+    // 设置拓扑排序的查找状态
     static final  int VISITING = 1, VISITED = 2;
     Map<Character, List<Character>> edges = new HashMap<>();
     Map<Character, Integer> states = new HashMap<>();
@@ -11,15 +12,19 @@ class Solution {
             int wordLength = word.length();
             for (int j = 0; j < wordLength; j++) {
                 char c = word.charAt(j);
+                // 找到所有的结点
                 edges.putIfAbsent(c, new ArrayList<>());
             }
         }
+        // 找到所有结点的有向边
         for (int i = 1; i < len; i++) {
             addEdge(words[i - 1], words[i]);
         }
+        // 模拟排序
         order = new char[edges.size()];
         index = edges.size() - 1;
         Set<Character> letterSet = edges.keySet();
+        // 如果没有开始查找就进行dfs
         for (char u : letterSet) {
             if (!states.containsKey(u)) {
                 dfs(u);
@@ -31,6 +36,7 @@ class Solution {
         return new String(order);
     }
 
+    // 查找所有字母的有向边
     public void addEdge(String before, String after) {
         int length1 = before.length(), length2 = after.length();
         int length = Math.min(length1, length2);
@@ -49,6 +55,7 @@ class Solution {
         }
     }
 
+    // 从某个结点开始深度遍历，判断是否有环路。
     public void dfs(char u){
         states.put(u, VISITING);
         List<Character> adjacent = edges.get(u);
