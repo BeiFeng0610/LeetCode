48d0586371fea73b603230911e1013b93e716aea
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-02 13:21:11 +0800

    Create Offer II 116. 省份数量.java

---
+++ 剑指Office（专项）/Offer II 116. 省份数量.java
@@ -0,0 +1,29 @@
+class Solution {
+    boolean[] tag;
+    int cityCount;
+    public int findCircleNum(int[][] isConnected) {
+        cityCount = isConnected.length;
+        tag = new boolean[cityCount];
+        int ret = 0;
+        for (int i = 0; i < cityCount; i++) {
+            if (!tag[i]) {
+                ret++;
+                dfs(isConnected, i);
+            }
+        }
+        return ret;
+    }
+
+    public void dfs(int[][] isConnected, int city) {
+        if (tag[city]) {
+            return;
+        }
+        tag[city] = true;
+        for (int i = 0; i < cityCount; i++) {
+            if (i != city && isConnected[city][i] == 1) {
+                dfs(isConnected, i);
+            }
+        }
+
+    }
+}
