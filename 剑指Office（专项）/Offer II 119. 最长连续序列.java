09c32431f2f287b22a64121557fa2cc6dc95aa05
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-04 13:13:09 +0800

    Update Offer II 119. 最长连续序列.java

--- 剑指Office（专项）/Offer II 119. 最长连续序列.java
+++ 剑指Office（专项）/Offer II 119. 最长连续序列.java
@@ -5,11 +5,14 @@ class Solution {
             num_set.add(num);
         }
 
+        // 把所有的数字都放到set中去重
         int longestStreak = 0;
         for (int num : num_set) {
+            // 如果存在 num-1 的数，则有更长的连续序列，无需进入循环。
             if (!num_set.contains(num - 1)) {
                 int curNum = num;
                 int curStreak = 1;
+                // 如果set中存在 num+1 的数则最大长度 +1
                 while (num_set.contains(curNum + 1)) {
                     curNum += 1;
                     curStreak += 1;
