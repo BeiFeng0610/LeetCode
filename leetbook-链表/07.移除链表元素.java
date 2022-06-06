810326891a6e57ac0527e5a504be7bfd8fe81923
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-06 13:16:20 +0800

    Update 07.移除链表元素.java

--- leetbook-链表/07.移除链表元素.java
+++ leetbook-链表/07.移除链表元素.java
@@ -2,6 +2,7 @@ class Solution {
     public ListNode removeElements(ListNode head, int val) {
         ListNode tmp = new ListNode(-1, head);
         ListNode res = tmp;
+        // 不符合就跳过，符合删除
         while (tmp.next != null) {
             if (tmp.next.val == val) {
                 tmp.next = tmp.next.next;
