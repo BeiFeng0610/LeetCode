e0a5256df2e6d974b02e246b01224c36b1c49e2d
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-07 18:24:54 +0800

    Create 12.两数相加.java

---
+++ leetbook-链表/12.两数相加.java
@@ -0,0 +1,37 @@
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode ret = new ListNode(-1);
+        ListNode tail = ret;
+        int jin = 0;
+        while (l1 != null || l2 != null) {
+            ListNode node = new ListNode();
+
+            int tmp = 0;
+            if (l1 == null) {
+                tmp = l2.val + jin;
+                l2 = l2.next;
+            } else if (l2 == null) {
+                tmp = l1.val + jin;
+                l1 = l1.next;
+            } else {
+                tmp = l1.val + l2.val + jin;
+                l1 = l1.next;
+                l2 = l2.next;
+            }
+
+            int val = tmp >= 10 ? tmp - 10 : tmp;
+            jin = tmp >= 10 ? 1 : 0;
+            node.val = val;
+
+            tail.next = node;
+            tail = node;
+
+
+        }
+        if (jin != 0) {
+            tail.next = new ListNode(1);
+        }
+
+        return ret.next;
+    }
+}
