f0b91fe82568f0c60495e61e79cd6f598a068da1
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-07 13:51:47 +0800

    Update 08.奇偶链表.java

--- leetbook-链表/08.奇偶链表.java
+++ leetbook-链表/08.奇偶链表.java
@@ -6,6 +6,7 @@ class Solution {
         ListNode left = head, right = head.next;
         int count = 1;
         while (right.next != null) {
+            // 只需要把奇数位结点  断开连接  重新加到left后面即可
             if (count % 2 != 0) {
                 ListNode tmp = right.next;
                 right.next = tmp.next;
