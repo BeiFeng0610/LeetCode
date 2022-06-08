06ab4ec3c644294efe57707e4ce7b1c2c575b6be
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-08 13:27:33 +0800

    Update 13.扁平化多级双向链表.java

--- leetbook-链表/13.扁平化多级双向链表.java
+++ leetbook-链表/13.扁平化多级双向链表.java
@@ -6,6 +6,7 @@ class Solution {
         }
         nodes = new ArrayList<>();
         dfs(head);
+        // 根据集合中的结点，按照顺序依次连接，把子节点清空。
         for (int i = 0; i < nodes.size() - 1; i++) {
             Node prev = nodes.get(i);
             Node tail = nodes.get(i + 1);
@@ -17,6 +18,7 @@ class Solution {
         return head;
     }
 
+    // 深度优先遍历，把所有的结点全部按顺序放到集合中。
     public void dfs(Node node){
         if (node == null) {
             return;
