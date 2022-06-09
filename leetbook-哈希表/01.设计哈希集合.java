b4b0ea0dc53e8bcca9acc7fee6e430de08df7367
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-09 14:01:55 +0800

    Create 01.设计哈希集合.java

---
+++ leetbook-哈希表/01.设计哈希集合.java
@@ -0,0 +1,52 @@
+class MyHashSet {
+
+    private static final int BASE = 769;
+    private LinkedList[] data;
+
+    public MyHashSet() {
+        data = new LinkedList[BASE];
+        for (int i = 0; i < BASE; i++) {
+            data[i] = new LinkedList<Integer>();
+        }
+    }
+
+    public void add(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer elm = iterator.next();
+            if (elm == key) {
+                return;
+            }
+        }
+        data[h].offerLast(key);
+    }
+
+    public void remove(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer elm = iterator.next();
+            if (elm == key) {
+                data[h].remove(elm);
+                return;
+            }
+        }
+    }
+
+    public boolean contains(int key) {
+        int h = hash(key);
+        Iterator<Integer> iterator = data[h].iterator();
+        while (iterator.hasNext()) {
+            Integer elm = iterator.next();
+            if (elm == key) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private static int hash(int key) {
+        return key % BASE;
+    }
+}
