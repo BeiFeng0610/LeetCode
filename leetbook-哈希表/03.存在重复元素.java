c3d83c6b1b52130760da4e3c6bc2e3e93adbfaee
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-10 13:01:42 +0800

    Create 03.存在重复元素.java

---
+++ leetbook-哈希表/03.存在重复元素.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+         Set<Integer> set = new HashSet<Integer>();
+         for(int x : nums){
+             if(!set.add(x)){
+                 return true;
+             }
+         }
+         return false;
+    }
+}
