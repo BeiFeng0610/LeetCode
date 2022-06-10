236aa1f5e57f6baa468e1d54f71feb8b80a0a427
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-10 13:53:15 +0800

    Create 06.快乐数.java

---
+++ leetbook-哈希表/06.快乐数.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean isHappy(int n) {
+        Set<Integer> set = new HashSet<>();
+        while (n != 1 && !set.contains(n)) {
+            set.add(n);
+            n = pf(n);
+        }
+
+        return n==1;
+    }
+
+    private int pf(int i) {
+        int sum = 0;
+        while (i > 0) {
+            int d = i % 10;
+            i /= 10;
+            sum += d * d;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        new Solution().isHappy(19);
+    }
+}
+class Solution2 {
+
+    public int getNext(int n) {
+        int totalSum = 0;
+        while (n > 0) {
+            int d = n % 10;
+            n = n / 10;
+            totalSum += d * d;
+        }
+        return totalSum;
+    }
+
+    public boolean isHappy(int n) {
+        int slowRunner = n;
+        int fastRunner = getNext(n);
+        while (fastRunner != 1 && slowRunner != fastRunner) {
+            slowRunner = getNext(slowRunner);
+            fastRunner = getNext(getNext(fastRunner));
+        }
+        return fastRunner == 1;
+    }
+}
