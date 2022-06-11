a3f9b968ac5f3d59a4462a5339922e69c877b842
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-11 14:28:23 +0800

    Create 08.同构字符串.java

---
+++ leetbook-哈希表/08.同构字符串.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        int sLen = s.length(), tLen = t.length();
+        if (sLen != tLen) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>(sLen);
+        Set<Character> set = new HashSet<>();
+
+        for (int i = 0; i < sLen; i++) {
+            if (!map.containsKey(s.charAt(i)) && !set.contains(t.charAt(i))) {
+                map.put(s.charAt(i), t.charAt(i));
+                set.add(t.charAt(i));
+            } else if (map.get(s.charAt(i)) == null || map.get(s.charAt(i)) != t.charAt(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+class Solution2 {
+    public boolean isIsomorphic(String s, String t) {
+        for(int i = 0; i < s.length(); i++){
+            if(s.indexOf(s.charAt(i)) != t.indexOf(t.charAt(i))){
+                return false;
+            }
+        }
+        return true;
+    }
+}
