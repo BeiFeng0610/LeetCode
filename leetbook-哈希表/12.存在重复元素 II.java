b505c37602d25fc287130449868fcbd5f7b5dcd1
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-12 14:25:24 +0800

    Create 12.存在重复元素 II.java

---
+++ leetbook-哈希表/12.存在重复元素 II.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<Integer>();
+        int length = nums.length;
+        for (int i = 0; i < length; i++) {
+            if (i > k) {
+                set.remove(nums[i - k - 1]);
+            }
+            if (!set.add(nums[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
