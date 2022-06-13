4c32bced7f754e76cc92ae0a8ed54f3b375f8ca1
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-13 13:35:55 +0800

    Update 12.存在重复元素 II.java

--- leetbook-哈希表/12.存在重复元素 II.java
+++ leetbook-哈希表/12.存在重复元素 II.java
@@ -3,9 +3,11 @@ class Solution {
         Set<Integer> set = new HashSet<Integer>();
         int length = nums.length;
         for (int i = 0; i < length; i++) {
+            // i 大于 k 说明窗口大于k了，删除窗口的第一个元素
             if (i > k) {
                 set.remove(nums[i - k - 1]);
             }
+            // 如果窗口内包含，则true
             if (!set.add(nums[i])) {
                 return true;
             }
