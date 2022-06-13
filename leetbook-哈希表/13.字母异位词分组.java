4555ca8e3218714fbd4a07e86ff811db6d8401b2
BeiFeng0610 <68510415+BeiFeng0610@users.noreply.github.com>
2022-06-13 13:37:09 +0800

    Update 13.字母异位词分组.java

--- leetbook-哈希表/13.字母异位词分组.java
+++ leetbook-哈希表/13.字母异位词分组.java
@@ -4,6 +4,7 @@ class Solution {
 
         for (String str : strs) {
             int[] ints = strToInt(str);
+            // 拼接出来一个key
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < 26; i++) {
                 if (ints[i] != 0) {
@@ -21,6 +22,7 @@ class Solution {
 
     }
 
+    // 用数组标记字母个数和分布
     private int[] strToInt(String str){
         int[] ans = new int[26];
         for (int i = 0; i < str.length(); i++) {
@@ -34,6 +36,7 @@ class Solution2 {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<String, List<String>>();
         for (String str : strs) {
+            // 把所有的字符排序，就变成key了
             char[] array = str.toCharArray();
             Arrays.sort(array);
             String key = new String(array);
