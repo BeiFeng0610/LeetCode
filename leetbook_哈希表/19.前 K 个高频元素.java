acb02c5428f67d4e478ba62ea274896bdb6f772d
Beif610 <beif610@foxmail.com>
2022-06-15 14:16:13 +0800

    19.前 K 个高频元素.java

---
+++ leetbook_哈希表/19.前 K 个高频元素.java
@@ -0,0 +1,37 @@
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        int[] ret = new int[k];
+
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+        // 优先队列排序
+        PriorityQueue<int[]> queue = new PriorityQueue<>(new Comparator<int[]>() {
+            @Override
+            public int compare(int[] o1, int[] o2) {
+                return o1[1] - o2[1];
+            }
+        });
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
+            int num = entry.getKey(), count = entry.getValue();
+            if (queue.size() == k) {
+                if (queue.peek()[1] < count) {
+                    queue.poll();
+                    queue.offer(new int[]{num, count});
+                }
+            } else {
+                queue.offer(new int[]{num, count});
+            }
+        }
+        for (int i = 0; i < k; i++) {
+            ret[i] = queue.poll()[0];
+        }
+        return ret;
+    }
+}
