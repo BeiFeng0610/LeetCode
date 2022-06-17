0995cf450bdb6152281490db4dc1f13a96d83eac
Beif610 <beif610@foxmail.com>
2022-06-17 14:11:32 +0800

    每日刷题

--- leetbook_查找表类算法/01.根据字符出现频率排序.java
+++ leetbook_查找表类算法/01.根据字符出现频率排序.java
@@ -13,16 +13,19 @@ class Solution {
         for (Character c : chars) {
             keyCount.put(c, keyCount.getOrDefault(c, 0) + 1);
         }
+        // 把字符按照出现顺序的降序放入优先队列
         PriorityQueue<int []> queue = new PriorityQueue<>(new Comparator<int[]>() {
             @Override
             public int compare(int[] o1, int[] o2) {
                 return o2[1] - o1[1];
             }
         });
+        // char  可以转换为 int
         for (Map.Entry<Character, Integer> entry : keyCount.entrySet()) {
             queue.offer(new int[]{entry.getKey(), entry.getValue()});
         }
 
+        // 然后挨个遍历拼接即可
         StringBuilder sb = new StringBuilder();
         while (!queue.isEmpty()) {
             int[] poll = queue.poll();
