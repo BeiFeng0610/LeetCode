1624d09d97294d557f5955e4726fea8690a91b3d
Beif610 <beif610@foxmail.com>
2022-06-28 14:58:18 +0800

    每日刷题

--- leetbook_二分查找/15.寻找旋转排序数组中的最小值 II.java
+++ leetbook_二分查找/15.寻找旋转排序数组中的最小值 II.java
@@ -7,6 +7,7 @@ class Solution {
         int l = 0, r = len - 1;
         while (l < r) {
             int mid = (r - l) / 2 + l;
+            // 两边相等，要么全相等，要么旋转过，找到一个不同的数
             if (nums[l] == nums[r]) {
                 l++;
             }else if (nums[mid] > nums[r]) {
---
+++ leetbook_二分查找/17.两个数组的交集 II.java
@@ -0,0 +1,57 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length > nums2.length) {
+            return intersect(nums2, nums1);
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums1) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        int[] ret = new int[nums1.length];
+        int index = 0;
+        for (int num : nums2) {
+            int count = map.getOrDefault(num, 0);
+            if (count > 0) {
+                ret[index++] = num;
+                count--;
+                if (count > 0) {
+                    map.put(num, count);
+                } else {
+                    map.remove(num);
+                }
+            }
+        }
+
+        return Arrays.copyOfRange(ret, 0, index);
+    }
+}
+
+class Solution2 {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        Arrays.sort(nums1);
+        Arrays.sort(nums2);
+
+        int len1 = nums1.length, len2 = nums2.length;
+        int[] ret = new int[Math.min(len1, len2)];
+        int idx1 = 0, idx2 = 0, idx = 0;
+        while (idx1 < len1 && idx2 < len2) {
+            if (nums1[idx1] < nums2[idx2]) {
+                idx1++;
+            } else if (nums1[idx1] > nums2[idx2]) {
+                idx2++;
+            } else {
+                ret[idx] = nums1[idx1];
+                idx1++;
+                idx2++;
+                idx++;
+            }
+        }
+        return Arrays.copyOfRange(ret, 0, idx);
+    }
+}
