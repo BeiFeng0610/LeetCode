e568ecbc6f27e2b1ecccff8780ec2c0779d999ca
Beif610 <beif610@foxmail.com>
2022-07-13 14:33:29 +0800

    每日刷题

---
+++ leetbook_前缀树/02.键值映射.java
@@ -0,0 +1,88 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class MapSum {
+
+    class TrieNode{
+        int val = 0;
+        TrieNode[] next = new TrieNode[26];
+    }
+
+    TrieNode root;
+    Map<String, Integer> map;
+
+    public MapSum() {
+        root = new TrieNode();
+        map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        TrieNode node = root;
+        for (char c : key.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                node.next[c - 'a'] = new TrieNode();
+            }
+            node = node.next[c - 'a'];
+            node.val += delta;
+        }
+    }
+
+    public int sum(String prefix) {
+        TrieNode node = root;
+        for (char c : prefix.toCharArray()) {
+            if (node.next[c - 'a'] == null) {
+                return 0;
+            }
+            node = node.next[c - 'a'];
+        }
+        return node.val;
+    }
+}
+
+class MapSum {
+    Map<String, Integer> map;
+
+    public MapSum() {
+        map = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        map.put(key,val);
+    }
+
+    public int sum(String prefix) {
+        int res = 0;
+        for (String s : map.keySet()) {
+            if (s.startsWith(prefix)) {
+                res += map.get(s);
+            }
+        }
+        return res;
+    }
+}
+
+class MapSum {
+    Map<String, Integer> map;
+    Map<String, Integer> prefixmap;
+
+    public MapSum() {
+        map = new HashMap<>();
+        prefixmap = new HashMap<>();
+    }
+
+    public void insert(String key, int val) {
+        int delta = val - map.getOrDefault(key, 0);
+        map.put(key, val);
+        for (int i = 1; i <= key.length(); ++i) {
+            String currprefix = key.substring(0, i);
+            prefixmap.put(currprefix, prefixmap.getOrDefault(currprefix, 0) + delta);
+        }
+    }
+
+    public int sum(String prefix) {
+        return prefixmap.getOrDefault(prefix, 0);
+    }
+}
+
