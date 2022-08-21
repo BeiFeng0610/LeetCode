03f0ad5c4db05ab1411d0dbe39b234194c9eeb60
Beif610 <beif610@foxmail.com>
2022-08-21 18:24:46 +0800

    每日刷题

---
+++ LeetCode_热题100/017.电话号码的字母组合.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    List<String> combinations;
+    Map<Character, String> phoneMap;
+    public List<String> letterCombinations(String digits) {
+        combinations = new ArrayList<>();
+        if (digits.length() == 0) {
+            return combinations;
+        }
+        phoneMap = new HashMap<>();
+        phoneMap.put('2', "abc");
+        phoneMap.put('3', "def");
+        phoneMap.put('4', "ghi");
+        phoneMap.put('5', "jkl");
+        phoneMap.put('6', "mno");
+        phoneMap.put('7', "pqrs");
+        phoneMap.put('8', "tuv");
+        phoneMap.put('9', "wxyz");
+        backtrack(digits, 0, new StringBuilder());
+        return combinations;
+    }
+
+    public void backtrack(String digits, int index,StringBuilder combination){
+        if (index == digits.length()) {
+            combinations.add(combination.toString());
+            return;
+        }
+        char digit = digits.charAt(index);
+        char[] chars = phoneMap.get(digit).toCharArray();
+        for (char aChar : chars) {
+            combination.append(aChar);
+            backtrack(digits, index + 1, combination);
+            combination.deleteCharAt(index);
+        }
+
+    }
+
+}
---
+++ LeetCode_热题100/019.删除链表的倒数第 N 个结点.java
@@ -0,0 +1,33 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (head == null) {
+            return head;
+        }
+        ListNode fast = new ListNode();
+        fast.next = head;
+        ListNode slow = new ListNode();
+        slow.next = head;
+
+        while (fast != null && n != 0) {
+            fast = fast.next;
+            n--;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        if (slow.next == head) {
+            return head.next;
+        } else {
+            slow.next = slow.next.next;
+        }
+        return head;
+    }
+}
