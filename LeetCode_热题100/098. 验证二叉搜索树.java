26452327f2385d96d38ee3421ffcb7ca953565c8
borei <borei.w@foxmail.com>
2022-09-10 15:10:25 +0800

    每日刷题

---
+++ LeetCode_热题100/098. 验证二叉搜索树.java
@@ -0,0 +1,51 @@
+import java.util.Deque;
+import java.util.LinkedList;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        long pre = Long.MIN_VALUE;
+
+        while (!stack.isEmpty() || root != null) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if (root.val <= pre) {
+                return false;
+            }
+            pre = root.val;
+            root = root.right;
+        }
+        return true;
+    }
+}
+
+class Solution {
+    public boolean isValidBST(TreeNode root) {
+        return dfs(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean dfs(TreeNode node, long min, long max) {
+        if (node == null) {
+            return true;
+        }
+        if (node.val <= min || node.val >= max) {
+            return false;
+        }
+        return dfs(node.left, min, node.val) && dfs(node.right, node.val, max);
+    }
+}
---
+++ LeetCode_热题100/101. 对称二叉树.java
@@ -0,0 +1,30 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        return dfs(root.left, root.right);
+    }
+
+    public boolean dfs(TreeNode node1, TreeNode node2) {
+        if (node1 == null && node2 == null) {
+            return true;
+        }
+        if (node1 == null || node2 == null) {
+            return false;
+        }
+        if (node1.val != node2.val) {
+            return false;
+        }
+        return dfs(node1.left, node2.right) && dfs(node1.right, node2.left);
+    }
+}
