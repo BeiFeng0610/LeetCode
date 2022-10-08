d10bdc5fe14d1beb5c1018136ea08d3fad7258e1
borei <borei.w@foxmail.com>
2022-10-08 15:00:32 +0800

    每日刷题

---
+++ LeetCode_热题100/538. 把二叉搜索树转换为累加树.java
@@ -0,0 +1,61 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int sum = 0;
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
+
+class Solution {
+    public TreeNode convertBST(TreeNode root) {
+        int sum = 0;
+        TreeNode node = root;
+
+        while (node != null) {
+            if (node.right == null) {
+                sum += node.val;
+                node.val = sum;
+                node = node.left;
+            } else {
+                TreeNode succ = getSuccessor(node);
+                if (succ.left == null) {
+                    succ.left = node;
+                    node = node.right;
+                } else {
+                    succ.left = null;
+                    sum += node.val;
+                    node.val = sum;
+                    node = node.left;
+                }
+            }
+        }
+
+        return root;
+    }
+
+    public TreeNode getSuccessor(TreeNode node) {
+        TreeNode succ = node.right;
+        while (succ.left != null && succ.left != node) {
+            succ = succ.left;
+        }
+        return succ;
+    }
+}
---
+++ LeetCode_热题100/543. 二叉树的直径.java
@@ -0,0 +1,30 @@
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {}
+    TreeNode(int val) { this.val = val; }
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    int res = 0;
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return res;
+    }
+
+    private int depth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        int left = depth(node.left);
+        int right = depth(node.right);
+        res = Math.max(res, left + right);
+        return Math.max(left, right) + 1;
+    }
+}
