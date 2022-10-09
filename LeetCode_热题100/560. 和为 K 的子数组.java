5a9d136c0cbb4ad6a56259db13415bdab16f727c
borei <borei.w@foxmail.com>
2022-10-09 16:17:17 +0800

    每日刷题

---
+++ LeetCode_热题100/560. 和为 K 的子数组.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int subarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int sum = 0, res = 0;
+        map.put(0, 1);
+        for (int num : nums) {
+            sum += num;
+            if (map.containsKey(sum - k)) {
+                res += map.get(sum - k);
+            }
+            map.put(sum, map.getOrDefault(sum, 0) + 1);
+        }
+        return res;
+    }
+}
---
+++ LeetCode_热题100/581. 最短无序连续子数组.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int findUnsortedSubarray(int[] nums) {
+        //初始化
+        int len = nums.length;
+        int min = nums[len-1];
+        int max = nums[0];
+        int begin = 0, end = -1;
+        //遍历
+        for(int i = 1; i < len; i++){
+            if(nums[i] < max){      //从左到右维持最大值，寻找右边界end
+                end = i;
+            }else{
+                max = nums[i];
+            }
+
+            if(nums[len-i-1] > min){    //从右到左维持最小值，寻找左边界begin
+                begin = len-i-1;
+            }else{
+                min = nums[len-i-1];
+            }
+        }
+        return end-begin+1;
+    }
+}
