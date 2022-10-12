3c86b270315c90fbb857a82889321fdecf6e489f
borei <borei.w@foxmail.com>
2022-10-12 15:53:43 +0800

    每日刷题

---
+++ LeetCode_精选_TOP_面试题/1. 两数之和.java
@@ -0,0 +1,30 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            int num = target - nums[i];
+            if (map.containsKey(num)) {
+                return new int[]{i, map.get(num)};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{-1, -1};
+    }
+}
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int len = nums.length;
+        for (int i = 0; i < len; i++) {
+            for (int j = i + 1; j < len; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
---
+++ LeetCode_精选_TOP_面试题/2. 两数相加.java
@@ -0,0 +1,33 @@
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode res = new ListNode();
+        ListNode tmp = res;
+        int carry = 0;
+        while (l1 != null || l2 != null || carry != 0) {
+            int sum = (l1 != null ? l1.val : 0) + (l2 != null ? l2.val : 0) + carry;
+            carry = sum >= 10 ? 1 : 0;
+            tmp.next = new ListNode(sum % 10);
+            tmp = tmp.next;
+            l1 = l1 == null ? null : l1.next;
+            l2 = l2 == null ? null : l2.next;
+        }
+        return res.next;
+    }
+}
