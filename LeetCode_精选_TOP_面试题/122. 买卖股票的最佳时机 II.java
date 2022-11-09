bc92b318b9e36e2160059d57b6a8d64bcfb7cea4
borei <borei.w@foxmail.com>
2022-11-09 10:29:38 +0800

    每日刷题

---
+++ LeetCode_精选_TOP_面试题/122. 买卖股票的最佳时机 II.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int max = 0;
+        for (int i = 0; i < prices.length; i++) {
+            if (min > prices[i]) {
+                min = prices[i];
+            } else if (prices[i] > min) {
+                max += prices[i] - min;
+                min = prices[i];
+            }
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        for (int i = 1; i < prices.length; i++) {
+            if (prices[i] > prices[i - 1]) {
+                max += prices[i] - prices[i - 1];
+            }
+        }
+        return max;
+    }
+}
