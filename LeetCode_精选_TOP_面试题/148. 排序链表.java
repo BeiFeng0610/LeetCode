61bf32704b72238127228beeebd64290798179c6
borei <borei.w@foxmail.com>
2022-11-17 09:55:23 +0800

    每日刷题

---
+++ LeetCode_精选_TOP_面试题/148. 排序链表.java
@@ -0,0 +1,107 @@
+import java.util.List;
+import java.util.PriorityQueue;
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        PriorityQueue<ListNode> queue = new PriorityQueue<>((o1, o2) -> o1.val - o2.val);
+        while (head != null) {
+            queue.offer(head);
+            head = head.next;
+        }
+        ListNode res = new ListNode();
+        ListNode tmp = res;
+        while (!queue.isEmpty()) {
+            tmp.next = queue.poll();
+            tmp = tmp.next;
+            tmp.next = null;
+        }
+
+        return res.next;
+    }
+}
+
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        return sortList(head, null);
+    }
+
+    public ListNode sortList(ListNode head, ListNode tail) {
+        if (head == null) {
+            return head;
+        }
+        if (head.next == tail) {
+            head.next = null;
+            return head;
+        }
+        ListNode slow = head, fast = head;
+        while (fast != tail) {
+            slow = slow.next;
+            fast = fast.next;
+            if (fast != tail) {
+                fast = fast.next;
+            }
+        }
+
+        ListNode mid = slow;
+        ListNode list1 = sortList(head, mid);
+        ListNode list2 = sortList(mid, tail);
+        return merge(list1, list2);
+    }
+
+    private ListNode merge(ListNode list1, ListNode list2) {
+        if (list1 == null) {
+            return list2;
+        }
+        if (list2 == null) {
+            return list1;
+        }
+        if (list1.val < list2.val) {
+            list1.next = merge(list1.next, list2);
+            return list1;
+        }
+        list2.next = merge(list1, list2.next);
+        return list2;
+    }
+}
+
+class Solution {
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        //递归，操作两个返回的头节点
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        //返回虚拟头节点的下一个
+        return res.next;
+    }
+}
