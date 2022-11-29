535c469dfbf911ef26bd305b4fae0e045e5264fa
Beif610 <beif610@foxmail.com>
2022-11-29 18:16:02 +0800

    每日刷题

---
+++ LeetCode_精选_TOP_面试题/206. 反转链表.java
@@ -0,0 +1,34 @@
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode res = new ListNode();
+        while (head != null) {
+            ListNode tmp = head;
+            head = head.next;
+            tmp.next = res.next;
+            res.next = tmp;
+        }
+        return res.next;
+    }
+}
+
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = reverseList(head.next);
+        head.next.next = head;
+        head.next = null;
+        return newHead;
+    }
+}
