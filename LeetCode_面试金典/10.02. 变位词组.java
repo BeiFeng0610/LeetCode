c39dc682f35af8eea31b5fec9ea4b81133fe6c57
Beif610 <beif610@foxmail.com>
2023-02-18 11:36:15 +0800

    每日刷题

--- LeetCode_面试金典/10.02. 变位词组.java
+++ LeetCode_面试金典/10.02. 变位词组.java
@@ -14,3 +14,24 @@ class Solution {
         return new ArrayList<>(map.values());
     }
 }
+
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String encode = encode(str);
+            map.putIfAbsent(encode, new ArrayList<>());
+            map.get(encode).add(str);
+        }
+        return new ArrayList<>(map.values());
+    }
+
+    private String encode(String str) {
+        char[] ch = new char[26];
+        for (char c : str.toCharArray()) {
+            ch[c - 'a']++;
+        }
+        return new String(ch);
+    }
+}
