91fea88d944cff11a3eb76007939c429c1e06526
Beif610 <beif610@foxmail.com>
2023-02-18 11:31:42 +0800

    每日刷题

---
+++ LeetCode_面试金典/10.02. 变位词组.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            char[] chars = str.toCharArray();
+            Arrays.sort(chars);
+            String key = new String(chars);
+            List<String> list = map.getOrDefault(key, new ArrayList<>());
+            list.add(str);
+            map.put(key, list);
+        }
+        return new ArrayList<>(map.values());
+    }
+}
