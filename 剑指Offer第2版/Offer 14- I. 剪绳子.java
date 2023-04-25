83c5d4938481dbe907db6c53d2544526a04598d1
borei <borei.w@foxmail.com>
2023-04-25 13:50:06 +0800

    每日刷题

---
+++ 剑指Offer第2版/Offer 14- I. 剪绳子.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int cuttingRope(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 1;
+        for (int i = 3; i <= n; i++) {
+            for (int j = 1; j <= i/2; j++) {
+                dp[i] = Math.max(dp[i], Math.max(dp[j], j) * Math.max(dp[i - j], i - j));
+            }
+        }
+        return dp[n];
+    }
+}
+
+class Solution {
+    public int cuttingRope(int n) {
+        if (n <= 3) {
+            return n - 1;
+        }
+        int quotient = n / 3;
+        int remainder = n % 3;
+        if (remainder == 0) {
+            return (int) Math.pow(3, quotient);
+        } else if (remainder == 1) {
+            return (int) Math.pow(3, quotient - 1) * 4;
+        } else {
+            return (int) Math.pow(3, quotient) * 2;
+        }
+    }
+}
